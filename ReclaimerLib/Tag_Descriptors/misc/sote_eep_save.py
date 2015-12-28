eeb8dca0db1c0662927f3a986fd0f912667d7548
Moses <Moses@localhost>
2015-12-28 04:33:35 -0500

    misc tweaks

--- ReclaimerLib/Tag_Descriptors/misc/sote_eep_save.py
+++ ReclaimerLib/Tag_Descriptors/misc/sote_eep_save.py
@@ -21,20 +21,20 @@ class SotE_EEP_Definition(Tag_Def):
                 4:{ TYPE:UInt8, GUI_NAME:"Disruptor"},
                 }
     
-    Tag_Structure = {TYPE:Container, GUI_NAME:"SotE EEP Save File",
-                     0:{TYPE:Struct, NAME:"Header",
-                        0:{ TYPE:UInt8,  GUI_NAME:"EEP CRC 1", EDITABLE:False},
-                        #if this is not b'\xFF\x53\x68\x26' then the file wont load
-                        1:{ TYPE:UInt32, GUI_NAME:"Magic Constant 1",
-                            EDITABLE:False, DEFAULT:b'\xFF\x53\x68\x26'},
-                        2:{ TYPE:Enum8,  GUI_NAME:"File just beaten",
-                            0:{NAME:"No File"},
-                            1:{NAME:"File 1"},
-                            2:{NAME:"File 2"},
-                            3:{NAME:"File 3"},
-                            4:{NAME:"File 4"}
-                            }
-                        },
+    Tag_Structure = { TYPE:Container, GUI_NAME:"SotE EEP Save File",
+                      0:{TYPE:Struct, NAME:"Header",
+                         0:{ TYPE:UInt8,  GUI_NAME:"EEP CRC 1", EDITABLE:False},
+                         #if this is not b'\xFF\x53\x68\x26' then the file wont load
+                         1:{ TYPE:UInt32, GUI_NAME:"Magic Constant 1",
+                             EDITABLE:False, DEFAULT:b'\xFF\x53\x68\x26'},
+                         2:{ TYPE:Enum8,  GUI_NAME:"File just beaten",
+                             0:{NAME:"No File"},
+                             1:{NAME:"File 1"},
+                             2:{NAME:"File 2"},
+                             3:{NAME:"File 3"},
+                             4:{NAME:"File 4"}
+                             }
+                         },
                      1:{TYPE:Array, GUI_NAME:"Save Files", SIZE:4, MIN:4, MAX:4,
                         SUB_STRUCT:{TYPE:Struct, SIZE:122, GUI_NAME:"Save File",
                                        0:{ TYPE:UInt8, GUI_NAME:"File CRC 1", DEFAULT:0, EDITABLE:False },
@@ -89,14 +89,14 @@ class SotE_EEP_Definition(Tag_Def):
                                            },
                                        
                                        6:{ TYPE:Big_Enum, GUI_NAME:"Control Scheme", SIZE:3,
-                                           0:{GUI_NAME:"Standard", VALUE:0},
-                                           1:{GUI_NAME:"Standard(View Lock)", VALUE:0x10001},
-                                           2:{GUI_NAME:"Traditional", VALUE:0x020002},
-                                           3:{GUI_NAME:"Traditional(View Lock)", VALUE:0x030003},
-                                           4:{GUI_NAME:"Alternate", VALUE:0x040004},
-                                           5:{GUI_NAME:"Alternate(View Lock)", VALUE:0x050005},
-                                           6:{GUI_NAME:"Strafe", VALUE:0x060006},
-                                           7:{GUI_NAME:"Strafe(View Lock)", VALUE:0x070007}
+                                           0:{GUI_NAME:"Standard",               VALUE:0x00000},
+                                           1:{GUI_NAME:"Standard(View Lock)",    VALUE:0x10001},
+                                           2:{GUI_NAME:"Traditional",            VALUE:0x20002},
+                                           3:{GUI_NAME:"Traditional(View Lock)", VALUE:0x30003},
+                                           4:{GUI_NAME:"Alternate",              VALUE:0x40004},
+                                           5:{GUI_NAME:"Alternate(View Lock)",   VALUE:0x50005},
+                                           6:{GUI_NAME:"Strafe",                 VALUE:0x60006},
+                                           7:{GUI_NAME:"Strafe(View Lock)",      VALUE:0x70007}
                                            },
                                        7:{ TYPE:Big_Enum, GUI_NAME:"Button Mapping", SIZE:3,
                                            0:{GUI_NAME:"Standard", VALUE:0x000000},
@@ -128,56 +128,56 @@ class SotE_EEP_Definition(Tag_Def):
                                            },
                                        10:{ PAD:1 },
                                        11:{ TYPE:Struct, SIZE:20, GUI_NAME:"Completion Times",
-                                           #measured in seconds
-                                           0:{ TYPE:UInt16, GUI_NAME:"Battle of Hoth"},
-                                           1:{ TYPE:UInt16, GUI_NAME:"Escape from Echo Base"},
-                                           2:{ TYPE:UInt16, GUI_NAME:"The Asteroid Field"},
-                                           3:{ TYPE:UInt16, GUI_NAME:"Ord Mantell Junkyard"},
-                                           4:{ TYPE:UInt16, GUI_NAME:"Gall Spaceport"},
-                                           5:{ TYPE:UInt16, GUI_NAME:"Mos Eisley and Beggars Canyon"},
-                                           6:{ TYPE:UInt16, GUI_NAME:"Imperial Freighter Suprosa"},
-                                           7:{ TYPE:UInt16, GUI_NAME:"Sewers of the Imperial City"},
-                                           8:{ TYPE:UInt16, GUI_NAME:"Xizors Palace"},
-                                           9:{ TYPE:UInt16, GUI_NAME:"Skyhook Battle"}
-                                           },
+                                            #measured in seconds
+                                            0:{ TYPE:UInt16, GUI_NAME:"Battle of Hoth" },
+                                            1:{ TYPE:UInt16, GUI_NAME:"Escape from Echo Base" },
+                                            2:{ TYPE:UInt16, GUI_NAME:"The Asteroid Field" },
+                                            3:{ TYPE:UInt16, GUI_NAME:"Ord Mantell Junkyard" },
+                                            4:{ TYPE:UInt16, GUI_NAME:"Gall Spaceport" },
+                                            5:{ TYPE:UInt16, GUI_NAME:"Mos Eisley and Beggars Canyon" },
+                                            6:{ TYPE:UInt16, GUI_NAME:"Imperial Freighter Suprosa" },
+                                            7:{ TYPE:UInt16, GUI_NAME:"Sewers of the Imperial City" },
+                                            8:{ TYPE:UInt16, GUI_NAME:"Xizors Palace" },
+                                            9:{ TYPE:UInt16, GUI_NAME:"Skyhook Battle" }
+                                            },
                                        12:{ TYPE:Struct, SIZE:10, GUI_NAME:"Challenge Points",
                                             0:{ GUI_NAME:"Battle of Hoth",
-                                                TYPE:UInt8, MIN:0, MAX:3},
+                                                TYPE:UInt8, MIN:0, MAX:3 },
                                             1:{ GUI_NAME:"Escape from Echo Base",
-                                                TYPE:UInt8, MIN:0, MAX:10},
+                                                TYPE:UInt8, MIN:0, MAX:10 },
                                             2:{ GUI_NAME:"The Asteroid Field",
-                                                TYPE:UInt8, MIN:0, MAX:6},
+                                                TYPE:UInt8, MIN:0, MAX:6 },
                                             3:{ GUI_NAME:"Ord Mantell Junkyard",
-                                                TYPE:UInt8, MIN:0, MAX:12},
+                                                TYPE:UInt8, MIN:0, MAX:12 },
                                             4:{ GUI_NAME:"Gall Spaceport",
-                                                TYPE:UInt8, MIN:0, MAX:15},
+                                                TYPE:UInt8, MIN:0, MAX:15 },
                                             5:{ GUI_NAME:"Mos Eisley and Beggars Canyon",
-                                                TYPE:UInt8, MIN:0, MAX:12},
+                                                TYPE:UInt8, MIN:0, MAX:12 },
                                             6:{ GUI_NAME:"Imperial Freighter Suprosa",
-                                                TYPE:UInt8, MIN:0, MAX:10},
+                                                TYPE:UInt8, MIN:0, MAX:10 },
                                             7:{ GUI_NAME:"Sewers of the Imperial City",
-                                                TYPE:UInt8, MIN:0, MAX:9},
+                                                TYPE:UInt8, MIN:0, MAX:9 },
                                             8:{ GUI_NAME:"Xizors Palace",
-                                                TYPE:UInt8, MIN:0, MAX:10},
+                                                TYPE:UInt8, MIN:0, MAX:10 },
                                             9:{ GUI_NAME:"Skyhook Battle",
-                                                TYPE:UInt8, MIN:0, MAX:0}
-                                           },
-                                       #Adding this value to the first checksum byte must equate to 255.
+                                                TYPE:UInt8, MIN:0, MAX:0 }
+                                            },
+                                       #OR-ing this value with the first checksum byte must produce 255, b'\xff'.
                                        #If it doesnt then the save is considered corrupt and is erased.
                                        #The first checksum byte will be defaulted to 0, and this one to 255.
-                                       13:{ TYPE:UInt8, GUI_NAME:"File CRC 2", DEFAULT:255, EDITABLE:False},
+                                       13:{ TYPE:UInt8, GUI_NAME:"File CRC 2", DEFAULT:255, EDITABLE:False },
                                        14:{ PAD:1 }
                                        }
                         },
-                     2:{TYPE:Struct, GUI_NAME:"Footer",
-                        #if this is not b'\x53\x68\x26' then the file wont load
-                        0:{ TYPE:Big_UInt, GUI_NAME:"Magic Constant 2",
-                            EDITABLE:False, SIZE:3, DEFAULT:b'\x53\x68\x26'},
-                        
-                        #Adding this value to the first checksum byte must equate to 255.
-                        #If it doesnt then the save is considered corrupt and is erased.
-                        #The first checksum byte will be defaulted to 0, and this one to 255.
-                        1:{ TYPE:UInt8, GUI_NAME:"EEP CRC 2", DEFAULT:255, EDITABLE:False},
-                        2:{ PAD:14 }
-                        }
+                     2:{ TYPE:Struct, GUI_NAME:"Footer",
+                         #if this is not b'\x53\x68\x26' then the file wont load
+                         0:{ TYPE:Big_UInt, GUI_NAME:"Magic Constant 2",
+                             EDITABLE:False, SIZE:3, DEFAULT:b'\x53\x68\x26' },
+                         
+                         #OR-ing this value with the first checksum byte must produce 255, b'\xff'.
+                         #If it doesnt then the save is considered corrupt and is erased.
+                         #The first checksum byte will be defaulted to 0, and this one to 255.
+                         1:{ TYPE:UInt8, GUI_NAME:"EEP CRC 2", DEFAULT:255, EDITABLE:False },
+                         2:{ PAD:14 }
+                         }
                      }
