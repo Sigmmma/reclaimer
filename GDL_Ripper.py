6b5ecfe355922f6b350ddb32ed696bce52e8eed0
Moses <29311834+MosesofEgypt@users.noreply.github.com>
2016-05-08 03:03:34 -0400

    Added ability to rip textures from gdl files Commented out most of the structures in objects.ps2 since it is very     time consuming and tedious to try and parse the data streams upon load. Created GDL_Ripper app Finished model extraction code for objects.ps2 Fixed bug where gauntlet's xbe def was not specified as incomplete

--- GDL_Ripper.py
+++ GDL_Ripper.py
@@ -5,14 +5,41 @@ try:
     from reclaimer.gdl.handler import GdlHandler
 
     test = GdlHandler(debug=3, valid_def_ids='objects', print_test=False)
-    input('Hit enter when ready to load.')
+    inp = ''
+    inp = input('Type in extraction operators:\n\n'+
+                '  d     == defs\n'+
+                '  t     == textures\n'+
+                '  m     == models\n'+
+                '  a     == animations\n\n'+
+                '  i     == individual models\n'+
+                '  ow    == overwrite\n'+
+                '  mips  == mipmaps\n'+
+                '  alpha == alpha palette\n>>> ').lower().split(' ')
+    cmd = set(c for c in ('i','m','a','d','t','o',
+                          'mips','alpha') if c in inp)
+    
+    while not cmd:
+        inp = input('Type in extraction operators:\n>>> ').lower().split(' ')
+        cmd = set(c for c in ('i','m','a','d','t','o',
+                              'mips','alpha') if c in inp)
+
+    print('Loading tags. Please wait...')
     
     test.load_tags_and_run()
     
     input('Hit enter when ready to rip.')
     
-    for filepath in test.tags['objects']:
-        test.tags['objects'][filepath].extract(True,True,True,True)
+    for filepath in sorted(test.tags['objects']):
+        print('extracting:', filepath)
+        test.tags['objects'][filepath].extract(defs='d' in cmd,
+                                               mod='m' in cmd,
+                                               tex='t' in cmd,
+                                               anim='a' in cmd,
+                                               individual='i' in cmd,
+                                               overwrite='ow' in cmd,
+                                               mips='mips' in cmd,
+                                               alpha_pal='alpha' in cmd)
+        del test.tags['objects'][filepath]
         
     input('Extraction finished.')
 except Exception:
