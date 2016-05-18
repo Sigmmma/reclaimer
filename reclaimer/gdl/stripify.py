c89b98f1011849c0c007d4c82990aa88d89d9d1c
Moses <Moses@localhost>
2016-05-18 00:09:48 -0400

    Finished stripify module     Finished link_strips function Misc tweaks and fixes

--- reclaimer/gdl/stripify.py
+++ reclaimer/gdl/stripify.py
@@ -6,30 +6,44 @@ DEFAULT_TEX = 0
 MAX_STRIP_LEN = 2**32-4
 
 class Stripifier():
+    ''''''
 
     #the max length a strip can be
     max_strip_len = MAX_STRIP_LEN
     
+    #whether or not the strips have all been linked together
+    linked = False
+    
+    #whether or not to link strips together with 2 degen tris
+    degen_link = True
+    
     def __init__(self, all_tris=None, *args, **kwargs):
-        ''''''
+        '''class initialization'''
         self.load_mesh(all_tris)
 
     
     def calc_strip(self, tri, neighbor_i=0, set_added=1):
-        ''''''
+        '''Given a starting triangle and the edge to start navigating,
+        this function will return a list of the verts that make up the
+        longest strip it can find and whether the strip faces backward.
+        
+        If set_added is True, triangles found will be flagged as
+        added to a strip. Otherwise they will be de-flagged.'''
         vert_data = self.vert_data
         #each triangle has 3 edges.
         #this is the index of the edge the
         #next triangle will be connected to
         neighbor_i = neighbor_i%3
         
-        strip_len = 0
+        strip_len = 2
         last_neighbor_i = neighbor_i
         strip_dir = False
         strip_reversed = False
 
         #keep track of which tris have been seen
         seen = set()
+        
+        set_added = bool(set_added)
         
         '''navigate the strip in reverse to find the best place to start'''
         while True:
@@ -109,62 +123,136 @@ class Stripifier():
     
 
     def link_strips(self):
-        ''''''
-        return
+        '''Links the strips that are currently loaded together into one
+        large strip. This will introduce degenerate triangles into the
+        mesh to link strips. 
+        
+        If degen_link is True, 2-3 additional degen triangles will be added 
+        between strips. Otherwise, 0-1 degen triangles will be added.
+        The variance depends on both strips directions and strip0's length.'''
+        if self.linked:
+            return
+        
         all_strips = self.all_strips
         all_degens = self.all_degens
         all_face_dirs = self.all_face_dirs
 
+        max_len = self.max_strip_len
+        degen_link = bool(self.degen_link)
+        
+        degen_tris_added = 2*degen_link
+
         for tex_index in all_strips:
             strips = all_strips[tex_index]
             face_dirs = all_face_dirs[tex_index]
+
+            #if there are no strips for this mesh, skip it
+            if not len(strips):
+                continue
             
             #a mapping that stores the strips from shortest to longest
             sorted_strips = {}
             sorted_dirs = {}
 
-            #if there are no strips for this mesh, skip it
-            if not len(strips):
-                continue
-
             #sort the strips and their facing directions by length
             for i in range(len(strips)):
-                strip = strips[i]
+                strip     = strips[i]
+                face_dir  = face_dirs[i]
                 strip_len = len(strip)
                 
                 same_len_strips = sorted_strips.get(strip_len, [])
                 same_len_strip_dirs = sorted_dirs.get(strip_len, [])
                 
-                same_len_strips.append(strips[i])
-                same_len_strip_dirs.append(face_dirs[i])
+                same_len_strips.append(strip)
+                same_len_strip_dirs.append(face_dir)
                 
                 sorted_strips[strip_len] = same_len_strips
                 sorted_dirs[strip_len]   = same_len_strip_dirs
 
-            #make a list to hold the new strips and dirs after they're linked
-            new_degens    = []
-            new_strips    = []
+            #make lists to hold the new strips, degens, and dirs
+            new_degens = []
+            new_strips = []
             new_face_dirs = []
 
-            strip_lengths = sorted(sorted_strips.keys())
+            strip_lengths = list(sorted(sorted_strips.keys()))
             
             #get the first set of strips to link together
-            curr_strips = sorted_strips[strip_lengths[0]]
-
+            strips_i = strip_lengths.pop(0)
+            curr_strips = sorted_strips.pop(strips_i)
+            curr_dirs   = sorted_dirs.pop(strips_i)
+            
             #get the first strip to link together
-            strip0 = curr_strips.pop()
-
+            strip0_dir = curr_dirs.pop(0)
+            strip0 = curr_strips.pop(0)
             
             '''keep linking strips together till none are left'''
-            while len(curr_strips):
-                new_degens.append([])
+            while strip0 is not None:
+                #make a new degens list and get the current strip direction
+                strip0_degens = []
+                new_degens.append(strip0_degens)
+                new_face_dirs.append(strip0_dir)
+                new_strips.append(strip0)
+
+                #link strips together until their length is maxed
+                while True:
+                    try:
+                        #if the set of strips has run out, get the next set
+                        if not len(curr_strips):
+                            strips_i = strip_lengths.pop(0)
+                            curr_strips = sorted_strips.pop(strips_i)
+                            curr_dirs   = sorted_dirs.pop(strips_i)
+                    except (KeyError, IndexError):
+                        strip1 = strip1_dir = None
+                        break
+
+                    #get the strip to link to
+                    strip1_dir = curr_dirs.pop(0)
+                    strip1 = curr_strips.pop(0)
+                    
+                    len0 = len(strip0)
+                    len1 = len(strip1)
+                    end_dir0 = (strip0_dir == (len0 % 2))
+                    '''find out if the strips face the opposite direction
+                    because if they do they'll need a degen between them'''
+                    add_degen = (end_dir0 == strip1_dir)
+                    
+                    #if the strip being added is empty, skip it
+                    if len1 == 0:
+                        continue
+                    
+                    if len0 + len1 + add_degen + degen_tris_added > max_len:
+                        #total length will be over max. dont try to combine
+                        break
+                    
+                    #add to the degens list
+                    for i in range(2+degen_tris_added+add_degen):
+                        strip0_degens.append(len(strip0)+i)
+
+                    #if a degen needs to be added, repeat the last vert
+                    if add_degen:
+                        strip0.append(strip0[-1])
+                    
+                    #create the extra degenerate triangles
+                    if degen_link:
+                        strip0.append(strip0[-1])
+                        strip1.insert(0, strip1[0])
+
+                    #merge the strips
+                    strip0.extend(strip1)
+
+                #restart the cycle on the next strip
+                strip0 = strip1
+                strip0_dir = strip1_dir
 
             all_degens[tex_index]    = new_degens
             all_strips[tex_index]    = new_strips
             all_face_dirs[tex_index] = new_face_dirs
+        
+        self.linked = True
 
 
     def load_mesh(self, all_tris=None):
+        '''Loads a list of triangles into the stripifier.'''
         #Maps each unique vert/uv/norm/color combo to
         #the index it is stored in in vert_data
         self.vert_map = vert_map = {}
@@ -190,6 +278,9 @@ class Stripifier():
         '''Stores the indexes of the degenerate triangles for each strip
         in each tex_index'''
         self.all_degens = {}
+
+        #whether or not the strips have been linked together
+        self.linked = False
         
         if all_tris is None:
             return
@@ -261,6 +352,8 @@ class Stripifier():
 
     
     def make_strips(self):
+        '''Takes all loaded triangles and
+        creates triangle strips out of them.'''
         all_tris_by_edges = self.all_tris_by_edges
         
         '''loop over all meshes by texture'''
