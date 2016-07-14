0df127dfb1bf607ea7c7a64b9510b1de745fefc8
Moses <Moses@localhost>
2016-07-14 04:43:37 -0400

    Tweak to the way 'ext' works for Tags

--- reclaimer/halo/hek/defs/objs/schi.py
+++ reclaimer/halo/hek/defs/objs/schi.py
@@ -5,7 +5,7 @@ class SchiTag(HekTag):
     
     def convert_to_scex(self, mod_desc=False):
         '''Call this function to convert a SCHI tag to a SCEX tag'''
-        ext = 'shader_transparent_chicago_extended'
+        self.ext = ext = 'shader_transparent_chicago_extended'
         self.data.blam_header.tag_class.set_to(ext)
         self.filepath = splitext(self.filepath)[0] + '.' + ext
         
