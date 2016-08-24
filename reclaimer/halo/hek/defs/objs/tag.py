5b2d9541f7f9e17aaa1fafa9267dede3f94d6185
Moses <29311834+MosesofEgypt@users.noreply.github.com>
2016-08-23 23:42:36 -0400

    Fixed HekTag.calc_pointers not being set Updated usage of backup_and_rename_temp in gdl.objects.objects.ObjectsPs2Tag.serialize

--- reclaimer/halo/hek/defs/objs/tag.py
+++ reclaimer/halo/hek/defs/objs/tag.py
@@ -3,7 +3,7 @@ from supyr_struct.tag import *
 
 class HekTag(Tag):
     def __init__(self, **kwargs):
-        calc_pointers = False
+        self.calc_pointers = False
         
         #this is used by various things to store variables
         #per tag which specify how it is to be changed.
