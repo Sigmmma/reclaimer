3c6f5afdd604d4e84ae572a729c2bb6eafe3dd35
Moses <29311834+MosesofEgypt@users.noreply.github.com>
2016-08-26 22:49:13 -0400

    Changed the formatting of the arguments for readers and writers     This includes making a 'block' argument so parent doesnt serve a double purpose Changed all calls of readers and writers to match new format Misc tweaks and fixes

--- Halo Bitmap Optimizer & Converter.py
+++ Halo Bitmap Optimizer & Converter.py
@@ -2,7 +2,9 @@ from traceback import format_exc
 try:
     from reclaimer.halo.hek.programs.bitmap_optimizer_and_converter import *
 
-    converter = BitmapConverter(debug = 5)
+    # setting debug to 1 or higher will disable log file creation,
+    # and instead make it so it is always printed to the console.
+    converter = BitmapConverter(debug = 0)
 
 except:
     print(format_exc())
