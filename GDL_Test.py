c7837cf1c53d6125f80e8ec112e7a0682aa0f2ae
Moses <Moses@localhost>
2016-09-14 00:06:13 -0400

    Changed stuff to match new naming conventions.

--- GDL_Test.py
+++ GDL_Test.py
@@ -12,7 +12,7 @@ try:
                        valid_def_ids=valid_id,
                        print_options={'indent':4,
                                       'printout':True, 'precision':3,
-                                      'show':['name', 'children', 'field',
+                                      'show':['name', 'subtrees', 'field',
                                               'value',#'offset',# 'py_id',
                                               'index', 'size','flags', # 'unique',
                                               'filepath', 'binsize','ramsize',
