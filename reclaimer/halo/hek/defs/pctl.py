1f7873a3c467045053a4afbb3444a58b38a6ea91
Moses <29311834+MosesofEgypt@users.noreply.github.com>
2016-11-05 02:36:17 -0400

    Implemented grhi, unhi, wphi, and pctl tags

---
+++ reclaimer/halo/hek/defs/pctl.py
@@ -0,0 +1,147 @@
+from ...common_descs import *
+from supyr_struct.defs.tag_def import TagDef
+
+particle_creation_physics = (
+    "default",
+    "explosion",
+    "jet"
+    )
+
+physics_constant = QStruct("physics constant",
+    BFloat("k"),
+    )
+
+state = Struct("state",
+    ascii_str32("name"),
+    QStruct("duration bounds", INCLUDE=from_to),
+    QStruct("transition time bounds", INCLUDE=from_to),
+
+    Pad(4),
+    BFloat("scale multiplier"),
+    BFloat("animation rate multiplier"),
+    BFloat("rotation rate multiplier"),
+    QStruct("color multiplier", INCLUDE=argb_float),
+    BFloat("radius multiplier"),
+    BFloat("minimum particle count"),
+    BFloat("particle creation rate"),  # particles/sec
+
+    Pad(84),
+    BSEnum16("particle creation physics", *particle_creation_physics),
+    Pad(2),  # BSEnum16("particle update physics", "default"),
+    reflexive("physics constants", physics_constant, 16),
+    SIZE=192
+    )
+
+particle_state = Struct("particle state",
+    ascii_str32("name"),
+    QStruct("duration bounds", INCLUDE=from_to),
+    QStruct("transition time bounds", INCLUDE=from_to),
+
+    dependency("bitmaps", valid_bitmaps),
+    BSInt16("sequence index"),
+
+    Pad(6),
+    QStruct("scale", INCLUDE=from_to),  # world units/sec
+    QStruct("animation rate", INCLUDE=from_to),  # frames/sec
+    QStruct("rotation rate", INCLUDE=from_to),  # radians/sec
+    QStruct("color 1", INCLUDE=argb_float),
+    QStruct("color 2", INCLUDE=argb_float),
+    BFloat("radius multiplier"),
+    dependency("physics", valid_point_physics),
+
+    Pad(76),
+    BBool16("shader flags", *shader_flags),
+    BSEnum16("framebuffer blend function", *framebuffer_blend_functions),
+    BSEnum16("framebuffer fade mode", *render_fade_mode),
+    BBool16("map flags",
+        "unfiltered"
+        ),
+
+    Pad(28),
+    #Secondary map
+    Struct("secondary map",
+        dependency("bitmap", valid_bitmaps),
+        BSEnum16("anchor", *render_anchor),
+        BBool16("flags",
+            "unfiltered"
+            ),
+        Struct("u-animation", INCLUDE=anim_src_func_per_pha_sca),
+        Struct("v-animation", INCLUDE=anim_src_func_per_pha_sca),
+        Struct("rotation-animation", INCLUDE=anim_src_func_per_pha_sca),
+        QStruct("rotation center", INCLUDE=xy_float),
+        Pad(4),
+        BFloat("zsprite radius scale"),
+        ),
+
+    Pad(20),
+    reflexive("physics constants", physics_constant, 16),
+    SIZE=376
+    )
+
+
+particle_type = Struct("particle type",
+    ascii_str32("name"),
+    BBool32("flags",
+        "type states loop",
+        "type states loops forward/backward",
+        "particle states loop",
+        "particle states loops forward/backward",
+        "particles die in water",
+        "particles die in air",
+        "particles die on ground",
+        "rotational sprites animate sideways",
+        "disabled",
+        "tint by effect color",
+        "initial count scales with effect",
+        "minimum count scales with effect",
+        "creation rate scales with effect",
+        "radius scales with effect",
+        "animation rate scales with effect",
+        "rotation rate scales with effect",
+        "dont draw in first person",
+        "dont draw in third person",
+        ),
+    BSInt16("initial particle count"),
+
+    Pad(2),
+    BSEnum16("complex sprite render modes",
+        "simple",
+        "rotational"
+        ),
+
+    Pad(2),
+    BFloat("radius"),
+
+    Pad(36),
+    BSEnum16("particle creation physics", *particle_creation_physics),
+
+    Pad(6),
+    reflexive("physics constants", physics_constant, 16),
+    reflexive("states", state, 8),
+    reflexive("particle states", particle_state, 8),
+    SIZE=128
+    )
+
+pctl_body = Struct("tagdata",
+    Pad(56),
+    dependency("point physics", valid_point_physics),
+    BSEnum16("system update physics",
+        "default",
+        "explosion"
+        ),
+    Pad(6),
+    reflexive("physics constants", physics_constant, 16),
+    reflexive("particle types", particle_type, 4),
+    SIZE=104,
+    )
+
+    
+def get():
+    return pctl_def
+
+pctl_def = TagDef("pctl",
+    blam_header("pctl", 4),
+    pctl_body,
+
+    ext=".particle_system", endian=">",
+    )
---
+++ reclaimer/halo/hek/defs/wphi.py
@@ -0,0 +1,329 @@
+from ...common_descs import *
+from supyr_struct.defs.tag_def import TagDef
+from .grhi import messaging_information, multitex_overlay, hud_background
+
+attached_state = BSEnum16("state attached to",
+    "total ammo",
+    "loaded ammo",
+    "heat",
+    "age",
+    "secondary weapon total ammo",
+    "secondary weapon loaded ammo",
+    "distance to target",
+    "elevation to target",
+    )
+
+use_on_map_type = BSEnum16("can use on map type",
+    "any",
+    "solo",
+    "multiplayer",
+    )
+
+static_element = Struct("static element",
+    attached_state,
+    Pad(2),
+    use_on_map_type,
+
+    Pad(30),
+    QStruct("anchor offset",
+        BSInt16("x"),
+        BSInt16("y"),
+        ),
+    BFloat("width scale"),
+    BFloat("height scale"),
+    BBool16("scaling flags", *hud_scaling_flags),
+
+    Pad(22),
+    dependency("interface bitmap", valid_bitmaps),
+    QStruct("default color", INCLUDE=argb_byte),
+    QStruct("flashing color", INCLUDE=argb_byte),
+    BFloat("flash period"),
+    BFloat("flash delay"),
+    BSInt16("number of flashes"),
+    BBool16("flash flags", *hud_flash_flags),
+    BFloat("flash length"),
+    QStruct("disabled color", INCLUDE=argb_byte),
+
+    Pad(4),
+    BSInt16("sequence index"),
+
+    Pad(2),
+    reflexive("multitex overlays", multitex_overlay, 30),
+    SIZE=180
+    )
+
+meter_element = Struct("meter element",
+    attached_state,
+    Pad(2),
+    use_on_map_type,
+
+    Pad(30),
+    QStruct("anchor offset",
+        BSInt16("x"),
+        BSInt16("y"),
+        ),
+    BFloat("width scale"),
+    BFloat("height scale"),
+    BBool16("scaling flags", *hud_scaling_flags),
+
+    Pad(22),
+    dependency("meter bitmap", valid_bitmaps),
+    Pad(1),
+    QStruct("color at meter minimum", INCLUDE=rgb_byte),
+    Pad(1),
+    QStruct("color at meter maximum", INCLUDE=rgb_byte),
+    Pad(1),
+    QStruct("flash color", INCLUDE=rgb_byte),
+    QStruct("empty color", INCLUDE=argb_byte),
+    Bool8("flags", *hud_panel_meter_flags),
+    SInt8("minimum meter value"),
+    BSInt16("sequence index"),
+    SInt8("alpha multiplier"),
+    SInt8("alpha bias"),
+    BSInt16("value scale"),
+    BFloat("opacity"),
+    BFloat("translucency"),
+    QStruct("disabled color", INCLUDE=argb_byte),
+    SIZE=180
+    )
+
+number_element = Struct("number element",
+    attached_state,
+    Pad(2),
+    use_on_map_type,
+
+    Pad(30),
+    QStruct("anchor offset",
+        BSInt16("x"),
+        BSInt16("y"),
+        ),
+    BFloat("width scale"),
+    BFloat("height scale"),
+    BBool16("scaling flags", *hud_scaling_flags),
+
+    Pad(22),
+    QStruct("default color", INCLUDE=argb_byte),
+    QStruct("flashing color", INCLUDE=argb_byte),
+    BFloat("flash period"),
+    BFloat("flash delay"),
+    BSInt16("number of flashes"),
+    BBool16("flash flags", *hud_flash_flags),
+    BFloat("flash length"),
+    QStruct("disabled color", INCLUDE=argb_byte),
+
+    Pad(4),
+    SInt8("maximum number of digits"),
+    Bool8("flags",
+        "show leading zeros",
+        "only show when zoomed",
+        "draw a trailing m",
+        ),
+    SInt8("number of fractional digits"),
+
+    Pad(14),
+    Bool8("weapon specific flags",
+        "divide number by magazine size"
+        ),
+    SIZE=160
+    )
+
+crosshair_overlay = Struct("crosshair overlay",
+    QStruct("anchor offset",
+        BSInt16("x"),
+        BSInt16("y"),
+        ),
+    BFloat("width scale"),
+    BFloat("height scale"),
+    BBool16("scaling flags", *hud_scaling_flags),
+
+    Pad(22),
+    QStruct("default color", INCLUDE=argb_byte),
+    QStruct("flashing color", INCLUDE=argb_byte),
+    BFloat("flash period"),
+    BFloat("flash delay"),
+    BSInt16("number of flashes"),
+    BBool16("flash flags", *hud_flash_flags),
+    BFloat("flash length"),
+    QStruct("disabled color", INCLUDE=argb_byte),
+
+    Pad(4),
+    BSInt16("frame rate"),
+    BSInt16("sequence index"),
+    BBool32("type",
+        "flashes when active",
+        "not a sprite",
+        "show only when zoomed",
+        "show sniper data",
+        "hide area outside reticle",
+        "one zoom level",
+        "dont show when zoomed",
+        ),
+    SIZE=108
+    )
+
+overlay = Struct("overlay",
+    QStruct("anchor offset",
+        BSInt16("x"),
+        BSInt16("y"),
+        ),
+    BFloat("width scale"),
+    BFloat("height scale"),
+    BBool16("scaling flags", *hud_scaling_flags),
+
+    Pad(22),
+    QStruct("default color", INCLUDE=argb_byte),
+    QStruct("flashing color", INCLUDE=argb_byte),
+    BFloat("flash period"),
+    BFloat("flash delay"),
+    BSInt16("number of flashes"),
+    BBool16("flash flags", *hud_flash_flags),
+    BFloat("flash length"),
+    QStruct("disabled color", INCLUDE=argb_byte),
+
+    Pad(4),
+    BSInt16("frame rate"),
+    Pad(2),
+    BSInt16("sequence index"),
+    BBool16("type",
+        "show on flashing",
+        "show on empty",
+        "show on reload/overheating",
+        "show on default",
+        "show always",
+        ),
+    BBool32("flags",
+        "flashes when active",
+        ),
+    SIZE=136
+    )
+
+crosshair = Struct("crosshair",
+    BSEnum16("crosshair type",
+        "aim",
+        "zoom",
+        "charge",
+        "should reload",
+        "flash heat",
+        "flash total ammo",
+        "flash battery",
+        "reload/overheat",
+        "flash when firing and no ammo",
+        "flash when throwing grenade and no grenade",
+        "low ammo and none left to reload",
+        "should reload secondary trigger",
+        "flash secondary total ammo",
+        "flash secondary reload",
+        "flash when firing secondary and no ammo",
+        "low secondary ammo and none left to reload",
+        "primary trigger ready",
+        "secondary trigger ready",
+        "flash when firing with depleted battery",
+        ),
+    Pad(2),
+    use_on_map_type,
+
+    Pad(30),
+    dependency("crosshair bitmap", valid_bitmaps),
+    reflexive("crosshair overlays", crosshair_overlay, 16),
+    SIZE=104
+    )
+
+overlay_element = Struct("overlay element",
+    attached_state,
+    Pad(2),
+    use_on_map_type,
+
+    Pad(30),
+    dependency("overlay bitmap", valid_bitmaps),
+    reflexive("overlays", overlay, 16),
+    SIZE=104
+    )
+
+screen_effect = Struct("screen effect",
+    Pad(5),
+    Struct("mask",
+        Bool8("flags",
+            "only when zoomed"
+            ),
+        Pad(18),
+        dependency("fullscreen mask", valid_bitmaps),
+        dependency("splitscreen mask", valid_bitmaps)
+        ),
+
+    Pad(9),
+    Struct("convolution",
+        Bool8("flags",
+            "only when zoomed"
+            ),
+        Pad(2),
+        QStruct("fov in bounds", INCLUDE=from_to),  # radians
+        QStruct("radius out bounds", INCLUDE=from_to)  # pixels
+        ),
+
+    Pad(25),
+    Struct("night vision",
+        Bool8("flags",
+            "only when zoomed",
+            "connect to flashlight",
+            "masked"
+            ),
+        BSInt16("script source", MIN=0, MAX=3),
+        BFloat("intensity", MIN=0, MAX=1)
+        ),
+
+    Pad(25),
+    Struct("desaturation",
+        Bool8("flags",
+            "only when zoomed",
+            "connect to flashlight",
+            "additive",
+            "masked"
+            ),
+        BSInt16("script source", MIN=0, MAX=3),
+        BFloat("intensity", MIN=0, MAX=1),
+        QStruct("tint", INCLUDE=rgb_float)
+        ),
+    SIZE=184
+    )
+
+wphi_body = Struct("tagdata",
+    dependency("child hud", "wphi"),
+    Struct("flash cutoffs",
+        BBool16("flags",
+            "use parent hud flashing parameters"
+            ),
+        Pad(2),
+        BSInt16("total ammo cutoff"),
+        BSInt16("loaded ammo cutoff"),
+        BSInt16("heat cutoff"),
+        BSInt16("age cutoff"),
+        ),
+
+    Pad(32),
+    BSEnum16("anchor", *hud_anchors),
+    
+    Pad(34),
+    reflexive("static elements", static_element, 16),
+    reflexive("meter elements", meter_element, 16),
+    reflexive("number elements", number_element, 16),
+    reflexive("crosshairs", crosshair, 19),
+    reflexive("overlay elements", overlay_element, 16),
+
+    Pad(16),
+    reflexive("screen effect", screen_effect, 1),
+
+    Pad(132),
+    messaging_information,
+    SIZE=380,
+    )
+
+    
+def get():
+    return wphi_def
+
+wphi_def = TagDef("wphi",
+    blam_header("wphi", 2),
+    wphi_body,
+
+    ext=".weapon_hud_interface", endian=">",
+    )
