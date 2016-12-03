54f20a25eef811d5667e49031f09acde064fb98d
Moses <Moses@localhost>
2016-12-03 02:18:41 -0500

    Made a config_def for mozzarilla

---
+++ reclaimer/halo/hek/programs/mozzarilla/config_def.py
@@ -0,0 +1,36 @@
+from supyr_struct.apps.binilla.config_def import *
+from supyr_struct.apps.binilla.constants import *
+from supyr_struct.field_types import *
+from supyr_struct.defs.tag_def import TagDef
+
+config_header = Struct("header",
+    UInt32("id", DEFAULT='ezoM'),
+    INCLUDE=config_header
+    )
+
+mozzarilla = Struct("mozzarilla",
+    Bool16("flags",
+        ),
+    UEnum16("selected_handler",
+        "halo_1",
+        "halo_1_os",
+        "halo_1_map",
+        "halo_1_misc",
+        ),
+    SIZE=128
+    )
+
+config_def = TagDef("mozzarilla_config",
+    config_header,
+    array_counts,
+    app_window,
+    widgets,
+    open_tags,
+    recent_tags,
+    directory_paths,
+    widget_depths,
+    colors,
+    hotkeys,
+    mozzarilla,
+    ENDIAN='<', ext=".cfg",
+    )
