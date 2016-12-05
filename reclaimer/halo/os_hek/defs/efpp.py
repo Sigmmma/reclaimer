3107016cf214fb4744d43ed17e97936d2ac3807d
Moses <Moses@localhost>
2016-12-04 21:03:39 -0500

    Fixes to the way open tags are recognized Implemented changing the tags directory Misc tweaks and fixes

--- reclaimer/halo/os_hek/defs/efpp.py
+++ reclaimer/halo/os_hek/defs/efpp.py
@@ -4,8 +4,7 @@ from supyr_struct.defs.tag_def import TagDef
 efpp_attrs = Struct("efpp attrs",
     Pad(12),
     QStruct("quad tesselation",
-        BSInt16("x"), BSInt16("y"),
-        ORIENT='h'
+        BSInt16("x"), BSInt16("y"), ORIENT='h'
         ),
     QStruct("x screen bounds", INCLUDE=from_to, MIN=0.0, MAX=1.0),
     QStruct("y screen bounds", INCLUDE=from_to, MIN=0.0, MAX=1.0),
