01fca6378e39d5d9f6629adb5f945386a8c76436
Moses <29311834+MosesofEgypt@users.noreply.github.com>
2016-12-18 23:26:51 -0500

    Modified senv for open sauce to use dynamic lightmaps Misc fixes and tweaks

--- reclaimer/halo/os_hek/defs/senv.py
+++ reclaimer/halo/os_hek/defs/senv.py
@@ -1,1 +1,53 @@
 from ...hek.defs.senv import *
+
+os_senv_ext = Struct("shader environment extension",
+    BBool16("flags",
+        "do not use dlms",
+        ),
+    Pad(2),
+    BFloat("bump amount"),
+
+    Struct("dlm color",
+        dependency_os("map", "bitm"),
+        BFloat("specular exponent"),
+        BFloat("specular coefficient"),
+        BBool16("flags",
+            "alpha as exponent mask",
+            ),
+        Pad(2),
+        ),
+
+    QStruct("dlm tint and brightness",
+        BFloat("perpendicular brightness"),
+        BFloat("perpendicular tint color"),
+        BFloat("parallel brightness"),
+        BFloat("parallel tint color"),
+        ),
+
+    QStruct("dlm intensity",
+        BFloat("lighting exponent"),
+        BFloat("lighting coefficient"),
+        ),
+
+    SIZE=100,
+    )
+
+# replace the padding with an open sauce shader environment extension reflexive
+senv_attrs = dict(senv_attrs)
+senv_attrs[3] = reflexive("os shader environment ext", os_senv_ext, 1)
+
+senv_body = Struct("tagdata",
+    shdr_attrs,
+    senv_attrs,
+    SIZE=836,
+    )
+
+def get():
+    return senv_def
+
+senv_def = TagDef("senv",
+    blam_header('senv', 2),
+    senv_body,
+
+    ext=".shader_environment", endian=">"
+    )
