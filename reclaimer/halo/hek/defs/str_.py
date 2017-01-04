9adb350dca73679c592ab8c9af4fe27e80fcbae7
Moses <29311834+MosesofEgypt@users.noreply.github.com>
2017-01-04 01:46:58 -0500

    Finished edit manager system Misc tweaks and fixes

--- reclaimer/halo/hek/defs/str_.py
+++ reclaimer/halo/hek/defs/str_.py
@@ -4,8 +4,7 @@ from supyr_struct.defs.tag_def import TagDef
 string_data_struct = rawtext_ref("string", StrLatin1, max_size=4096)
 
 str__body = Struct("tagdata",
-    reflexive("strings", string_data_struct, 32767,
-        widget=TextFrame, DYN_NAME_PATH='.data'),
+    reflexive("strings", string_data_struct, 32767, widget=TextFrame),
     SIZE=12,
     )
 
