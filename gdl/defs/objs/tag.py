e17699010aad2da80696e6c2c90cf265ec013366
Moses <29311834+MosesofEgypt@users.noreply.github.com>
2017-02-17 20:17:13 -0500

    Removed miscellaneous and gauntlet stuff from library, so now     reclaimer is purely a halo editing library     Also, for some reason it isnt detecting some of the ranames again...

--- gdl/defs/objs/tag.py
+++
@@ -1,7 +0,0 @@
-from supyr_struct.tag import *
-from supyr_struct.field_types import *
-
-
-class GdlTag(Tag):
-    def __init__(self, **kwargs):
-        Tag.__init__(self, **kwargs)
--- os_hek/handler.py
+++ os_hek/handler.py
@@ -5,7 +5,7 @@ from os.path import abspath
 from ..hek.handler import *
 
 
-class StubbsHandler(HaloHandler):
-    default_defs_path = "reclaimer.halo.stubbs.defs"
+class OsHaloHandler(HaloHandler):
+    default_defs_path = "reclaimer.os_hek.defs"
 
     tagsdir = "%s%stags%s" % (abspath(os.curdir), PATHDIV, PATHDIV)
