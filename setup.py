ab9c22f707aefed6309b5558da9d00e4384935b0
Moses <Moses@localhost>
2017-02-23 18:04:55 -0500

    Made it so ijkw_float has w set to 1.0 as default Fixed bug in matrices that would prevent it from allowing     non-iterables(int or float for example) as each row.

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2017.02.20"
-version = (1, 0, 2)
+release_date = "2017.02.23"
+version = (1, 0, 3)
 
 try:
     try:
