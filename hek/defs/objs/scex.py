a2288fc08ada24dab8b0e4e8aca87498df240fde
Moses <Moses@localhost>
2017-02-28 18:23:41 -0500

    Removed ability to modify descriptor from scex and schi tag convert methods.     This is due to XXX_desc methods being removed from Block in supyr_struct.

--- hek/defs/objs/scex.py
+++ hek/defs/objs/scex.py
@@ -3,7 +3,7 @@ from .tag import *
 
 class ScexTag(HekTag):
 
-    def convert_to_schi(self, mod_desc=False):    
+    def convert_to_schi(self):    
         '''Call this function to convert a SCEX tag to a SCHI tag'''
         self.ext = ext = 'shader_transparent_chicago'
         self.data.blam_header.tag_class.set_to(ext)
@@ -11,10 +11,3 @@ class ScexTag(HekTag):
         
         tagdata = self.data.tagdata
         tagdata.shdr_attrs.shader_type.data = 6
-        if mod_desc:
-            dict.__setitem__(tagdata.scex_attrs.two_stage_maps.desc,
-                             'TYPE', Void)
-            tagdata.scex_attrs.ATTR_OFFS[
-                tagdata.scex_attrs.NAME_MAP['extra_flags']] = 96
-            tagdata.set_size(108)
-            tagdata.scex_attrs.set_size(68)
