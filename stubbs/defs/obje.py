755663ec19b93599f53866dda3dbcb38a1a3c643
Moses <Moses@localhost>
2017-04-04 17:53:21 -0400

    Fixed stubbs actor tag not having the previous actr version fix Fixed stubbs obje(and all tags inheriting from it) having gbxmodel in     the possible model tag classes

--- stubbs/defs/obje.py
+++ stubbs/defs/obje.py
@@ -1,1 +1,19 @@
 from ...hek.defs.obje import *
+
+def get():
+    return obje_def
+
+obje_attrs = dict(obje_attrs)
+obje_attrs[7] = dependency('model', 'mode')
+
+obje_body = Struct('tagdata',
+    obje_attrs,
+    SIZE=380
+    )
+
+obje_def = TagDef("obje",
+    blam_header('obje'),
+    obje_body,
+
+    ext=".object", endian=">"
+    )
--- stubbs/defs/proj.py
+++ stubbs/defs/proj.py
@@ -1,8 +1,12 @@
 from ...hek.defs.proj import *
-from .obje import *
 from ..common_descs import *
-from .objs.tag import StubbsTag
+from .obje import *
 from supyr_struct.defs.tag_def import TagDef
+
+# replace the object_type enum one that uses
+# the correct default value for this object
+obje_attrs = dict(obje_attrs)
+obje_attrs[0] = dict(obje_attrs[0], DEFAULT=5)
 
 proj_attrs = dict(proj_attrs)
 proj_attrs[13] = reflexive("material responses", material_response,
