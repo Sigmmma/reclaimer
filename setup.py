328fcc1660b9b587080c2a67c2338d7e4bb08e98
Moses <Moses@localhost>
2017-04-09 13:31:11 -0400

    Fully implemented the first Halo 2 tag and many of the common_desc macros

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2017.04.05"
-version = (1, 0, 18)
+release_date = "2017.04.09"
+version = (1, 0, 19)
 
 try:
     try:
