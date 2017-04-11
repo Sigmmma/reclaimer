c98d60ac4e9dc768531160e14418203f834f0747
Moses <29311834+MosesofEgypt@users.noreply.github.com>
2017-04-10 23:27:54 -0400

    Added new, incomplete, tag definitions to h2v...    and decided to stop trying to make them

---
+++ reclaimer/h2v/defs/snd_.py
@@ -0,0 +1,212 @@
+from ..common_descs import *
+from .objs.tag import H2VTag
+from supyr_struct.defs.tag_def import TagDef
+
+sound_classes = (
+    ("projectile impact", 0),
+    ("projectile detonation", 1),
+    ("projectile flyby", 2),
+
+    ("weapon fire", 4),
+    ("weapon ready", 5),
+    ("weapon reload", 6),
+    ("weapon empty", 7),
+    ("weapon charge", 8),
+    ("weapon overheat", 9),
+    ("weapon idle", 10),
+    ("weapon melee", 11),
+    ("weapon animation", 12),
+    ("object impacts", 13),
+    ("particle impacts", 14),
+
+    ("unit footsteps", 18),
+    ("unit dialog", 19),
+    ("unit animation", 20),
+
+    ("vehicle collision", 22),
+    ("vehicle engine", 23),
+    ("vehicle animation", 20),
+
+    ("device door", 26),
+    ("device machinery", 28),
+    ("device stationary", 29),
+
+    ("music", 32),
+    ("ambient nature", 33),
+    ("ambient machinery", 34),
+
+    ("huge ass", 36),
+    ("object looping", 37),
+    ("cinematic music", 38),
+
+    ("cortana mission", 45),
+    ("cortana cinematic", 46),
+    ("mission dialog", 47),
+    ("cinematic dialog", 48),
+    ("scripted cinematic foley", 49),
+    ("game event", 50),
+    ("ui", 51),
+    ("test", 52),
+    ("multilingual test", 53),
+    )
+
+promotion_rule = Struct("promotion rule",
+    UEnum16("pitch ranges",
+        "",
+        ),
+    UInt16("maximum playing count"),
+    float_sec("supression time"),
+    )
+
+permutation_v0 = Struct("permutation",
+    ascii_str_varlen("name"),
+    Float("skip fraction"),
+    Float("gain"),
+    h2_rawdata_ref("samples", max_size=0),
+    SIZE=32
+    )
+'''
+permutation_v1 = Struct("permutation",
+    UEnum16("language",
+        "english",
+        "japanese",
+        "german",
+        "french",
+        "spanish",
+        "italian",
+        "korean",
+        "chinese",
+        "portugese",
+        ),
+    Pad(2),
+    unknown("play fraction"),
+    ascii_str_varlen("name"),
+    Float("skip fraction"),
+    Float("gain"),
+    h2_rawdata_ref("samples", max_size=0),
+    SIZE=32
+    )
+'''
+pitch_range = Struct("pitch range",
+    ascii_str_varlen("name"),
+    SInt16("natural pitch", SIDETIP="cents"),
+    Pad(2),
+    QStruct("bend bounds", INCLUDE=from_to_cents),
+    Pad(8),
+    h2_reflexive("permutations",
+        (permutation_v0, 32),
+        max_count=0
+        ),
+    SIZE=32
+    )
+
+platform_parameter = Struct("platform parameter",
+    )
+
+from_to_cents = QStruct("",
+    SInt16("from", GUI_NAME=''),
+    SInt16("to"),
+    SIDETIP="cents"
+    )
+
+BLM__body = Struct("tagdata",
+    Bool32("flags",
+        "fit to adpcm blocksize",
+        "split long sounds into permutations",
+        "always spatialize",
+        "never obstruct",
+        {NAME: "INTERNAL DONT TOUCH", EDITABLE=False},
+        "use huge sound transmission"
+        "link count to owner unit",
+        "pitch range is language",
+        "dont use sound class speaker flags",
+        "dont use lipsync data",
+        ),
+    UEnum8("class", *sound_classes),
+    UEnum8("sample rate",
+        {NAME: "khz_22", GUI_NAME: "22kHz"},
+        {NAME: "khz_44", GUI_NAME: "44kHz"},
+        {NAME: "khz_32", GUI_NAME: "32kHz"},
+        ),
+    SEnum16("import type",
+        "unknown",
+        "single shot",
+        "single layer",
+        "multi layer",
+        ),
+    float_wu("minimum distance"),
+    float_wu("maximum distance"),
+    float_zero_to_one("skip fraction"),
+    Float("maximum bend per second", SIDETIP="cents"),
+
+    Struct("randomization",
+        Float("gain base"),
+        Float("gain variance"),
+        QStruct("random pitch bounds", INCLUDE=from_to_cents),
+        ),
+
+    Struct("directional sounds",
+        float_rad("inner cone angle"),
+        float_rad("outer cone angle"),
+        Float("outer cone gain", SIDETIP="dB"),
+        ),
+
+    Struct("scripted location override",
+        Bool32("flags",
+            "override azimuth",
+            "override 3d gain",
+            "override speaker gain",
+            ),
+        Float("azimuth"),
+        Float("positional gain", SIDETIP="dB"),
+        Float("first person gain", SIDETIP="dB"),
+        ),
+
+    Struct("scale modifiers",
+        QStruct("gain", INCLUDE=from_to, SIDETIP="dB"),
+        QStruct("pitch", INCLUDE=from_to_cents),
+        QStruct("skip fraction", INCLUDE=from_to),
+        ),
+
+    Pad(2),
+    UEnum8("encoding",
+        "mono",
+        "stereo",
+        ),
+    UEnum8("compression",
+        "none (big endian)",
+        "xbox adpcm",
+        "ima adpcm",
+        "none (little endian)",
+        "wma",
+        ),
+
+    h2_reflexive("promotion rules",
+        (promotion_rule, ),
+        max_count=0
+        ),
+    Pad(12),  # h2_reflexive("unknown0", (unknown0_struct, ), max_count=0),
+    Pad(24),
+    h2_reflexive("pitch ranges",
+        (pitch_range, 32),
+        max_count=0
+        ),
+    h2_reflexive("platform parameters",
+        (platform_parameter, ),
+        max_count=0
+        ),
+    h2_reflexive("unknown1", (unknown1_struct, ), max_count=0),
+    SIZE=172
+    )
+
+
+def get():
+    return snd__def
+
+snd__def = TagDef("snd!",
+    h2v_blam_header('snd!'),
+    h2_tagdata_switch(
+        (BLM__body, 172, 'BLM_', 5)
+        ),
+    ext=".sound", endian="<", tag_cls=H2VTag
+    )
