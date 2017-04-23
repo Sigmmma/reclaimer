9cb2b441d8105b3557678a20ce10c9360e3879f7
Moses <29311834+MosesofEgypt@users.noreply.github.com>
2017-04-23 01:38:01 -0400

    Commented out the tagdata from the map Switch Organizing meta descriptors for eventual use in map parsing Misc tweaks and fixes

--- os_v3_hek/defs/meta_descs.py
+++ os_v3_hek/defs/meta_descs.py
@@ -1,10 +1,8 @@
-from ...hek.defs.meta_descs import *
+from ...os_hek.defs.meta_descs import *
 from . import *
 
 modules = locals()
 meta_cases = dict(meta_cases)
-fcc_map = dict(**fcc_map)
-fccs.remove("gelc")
 
 for fcc in fccs:
     meta_cases[fcc_map.get(fcc, fcc)] = modules[fcc].get().descriptor[1]
