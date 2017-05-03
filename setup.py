6390401d72a66ec85c4a4745a382d53c09811194
Moses <29311834+MosesofEgypt@users.noreply.github.com>
2017-05-03 03:23:42 -0400

    Started working on making circular dependencies of arbitrary complexity be hashable Misc tweaks and fixes

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2017.04.29"
-version = (1, 1, 6)
+release_date = "2017.05.03"
+version = (1, 1, 7)
 
 try:
     try:
