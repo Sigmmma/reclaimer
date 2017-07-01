6460ed354c71e43248451c998e4c83e5a9a19b13
Moses <29311834+MosesofEgypt@users.noreply.github.com>
2017-07-01 13:06:13 -0400

    Implemented a check to make sure seeking for a reflexive array cannot jump to     outside a mapfile

--- reclaimer/setup.py
+++ reclaimer/setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2017.06.10"
-version = (1, 2, 6)
+release_date = "2017.07.01"
+version = (1, 2, 7)
 
 try:
     try:
