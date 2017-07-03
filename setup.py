a0ab9ca3d7392d968dd2c8f2edaf4351e910a672
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2017-07-02 22:16:54 -0400

    Turned a handful of SInt16's in scnr into dyn_senum16  Implemented missing directional lightmaps in osv4 scnr

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2017.07.01"
-version = (1, 2, 8)
+release_date = "2017.07.02"
+version = (1, 2, 9)
 
 try:
     try:
