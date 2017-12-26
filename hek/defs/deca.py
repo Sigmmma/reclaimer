759dd5b4e091816f232a59a8c8bfa50b7378e87f
NESSY\Michelle <gbmichelle.dev@gmail.com>
2017-12-26 21:23:08 +0100

    Added the guerilla explanation field to the decal tag.

--- hek/defs/deca.py
+++ hek/defs/deca.py
@@ -2,6 +2,27 @@ from ...common_descs import *
 from .objs.tag import HekTag
 from supyr_struct.defs.tag_def import TagDef
 
+decal_comment = """COMPOUND DECALS:
+A 'compound decal' is a chain of decals which are instantiated simultaneously. 
+Compound decals are created by choosing a <next_decal_in_chain> below. 
+NOTE: Do not attempt to create a circularly linked decal chain, i.e. A->B->C->A! 
+Also, do not reference a decal from an effect if it is not the 'head' of the chain;
+for example an effect should not instantiate decal B if the chain was A->B->C. 
+Compound decals can have seperate bitmaps, seperate framebuffer blend functions, 
+and can be drawn in seperate layers. In addition, each decal in the chain can either 
+inherit its parent's <radius>, rotation, <color>, <fade>, and <sequence> - 
+or it can randomly choose its own. This behavior is controlled by the 
+'geometry_inherited_by_next_decal_in_chain' flag, below. 
+
+DECAL TYPING AND LAYERING:
+The decal <type> (or layer) determines the drawing order of the decal with respect 
+to the rest of the environment. Decals in the primary layer are drawn after the 
+environment diffuse texture, hence they affect the already-lit texture of the surface.
+Decals in the secondary layer are drawn immediately after decals in the primary layer,
+so they 'cover up' the primary decals. Decals in the 'light' layer are drawn before
+the environment diffuse texture, hence they affect the accumulated diffuse light and
+only indirectly affect the lit texture."""
+
 deca_body = Struct("tagdata",
     #Decal Properties
     Bool16("meter shader flags",
@@ -14,6 +35,7 @@ deca_body = Struct("tagdata",
         "SAPIEN-incremental counter",
         "animation loop",
         "preserve aspect",
+		COMMENT=decal_comment
         ),
     SEnum16("type",
         "scratch",
@@ -61,7 +83,7 @@ deca_body = Struct("tagdata",
     #Sprite info
     Pad(20),
     Float("maximum sprite extent", SIDETIP="pixels"),
-
+	
     SIZE=268,
     )
 
