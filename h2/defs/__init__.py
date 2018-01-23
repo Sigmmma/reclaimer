e72e9f635edc75d6d261fc5fef13642b402c8efb
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-01-23 03:19:45 -0500

    Started implementing a method for freezing definition module paths Fixed bugs in stubbs definitions

--- h2/defs/__init__.py
+++ h2/defs/__init__.py
@@ -1,16 +1,17 @@
+# DO NOT REMOVE! __all__ is used to locate frozen definitions
 __all__ = (
-    "adlg", "mdlg", "ant!", "bipd", "bitm", "bsdt", "$#!+", "trak", "devo",\
-    "whip", "char", "gldf", "clwd", "coll", "coln", "colo", "cont", "bloc",\
-    "crea", "jpt!", "deca", "DECR", "DECP", "dobc", "devi", "ctrl", "lifi",\
-    "mach", "udlg", "effe", "eqip", "garb", "matg", "grhi", "hudg", "hmt ",\
-    "hud#", "item", "itmc", "lens", "ligh", "MGS2", "tdtl", "foot", "mpdt",\
-    "metr", "hlmt", "jmad", "mcsr", "unic", "mulg", "mply", "goof", "nhdt",\
-    "obje", "prt3", "PRTM", "pmov", "fpch", "phys", "phmo", "pixl", "fog ",\
-    "pphy", "proj", "mode", "sbsp", "ltmp", "scnr", "ai**", "*ipd", "cin*",\
-    "clu*", "/**/", "*rea", "dec*", "dc*s", "dgr*", "*qip", "hsc*", "*cen",\
-    "*sce", "sslt", "*igh", "trg*", "*ehi", "*eap", "scen", "egor", "shad",\
-    "stem", "slit", "spas", "sky ", "snd!", "ugh!", "sncl", "spk!", "<fx>",\
-    "sfx+", "snde", "lsnd", "snmx", "ssce", "BooM", "styl", "sily", "unit",\
-    "unhi", "wgtz", "skin", "wgit", "wigl", "vehi", "vehc", "vrtx", "weap",\
-    "wphi", "weat", "wind", "wind"
+    "ugh_", "ant_", "bitm", "trak", "snd_", "shad", "hsc_", "pphy",
+    #"adlg", "mdlg", "bipd", "bsdt", "____", "devo", "whip", "char", "gldf",
+    #"clwd", "coll", "coln", "colo", "cont", "bloc", "crea", "jpt_", "deca",
+    #"DECR", "DECP", "dobc", "devi", "ctrl", "lifi", "mach", "udlg", "effe",
+    #"eqip", "garb", "matg", "grhi", "hudg", "hmt_", "hud_", "item", "itmc",
+    #"lens", "ligh", "MGS2", "tdtl", "foot", "mpdt", "metr", "hlmt", "jmad",
+    #"mcsr", "unic", "mulg", "mply", "goof", "nhdt", "obje", "prt3", "PRTM",
+    #"pmov", "fpch", "phys", "phmo", "pixl", "fog ", "proj", "mode", "sbsp",
+    #"ltmp", "scnr", "ai__", "_ipd", "cin_", "clu_", "____", "_rea", "dec_",
+    #"dc_s", "dgr_", "_qip", "_cen", "_sce", "sslt", "_igh", "trg_", "_ehi",
+    #"_eap", "scen", "egor", "stem", "slit", "spas", "sky_", "sncl", "spk_",
+    #"sfx_", "snde", "lsnd", "snmx", "ssce", "BooM", "styl", "sily", "unit",
+    #"unhi", "wgtz", "skin", "wgit", "wigl", "vehi", "vehc", "vrtx", "weap",
+    #"_fx_", "wphi", "weat", "wind"
     )
