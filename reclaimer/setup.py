78a3c82d8a3f5106915e8d2cdf6a8af02f5a4329
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-02-18 23:19:46 -0500

    Jeez, really? I defined these flags as an SInt8

--- reclaimer/setup.py
+++ reclaimer/setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2018.02.07"
-version = (1, 8, 6)
+release_date = "2018.02.18"
+version = (1, 8, 7)
 
 try:
     try:
