e1d6a6ec2c4241bac63d9a4c030967a1cab92048
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-06-14 17:39:48 -0400

    Fixed bug with xbox bsps crashing sapien when switching bsps

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2018.06.12"
-version = (1, 9, 5)
+release_date = "2018.06.14"
+version = (1, 9, 6)
 
 try:
     try:
