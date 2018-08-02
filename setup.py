3acbb00ef57cb750b9f9cc0136ad37cdd4fc5bcb
Moses_of_Egypt <Moses_of_Egypt@localhost>
2018-08-02 13:48:01 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2018.07.28"
-version = (1, 9, 9)
+release_date = "2018.08.02"
+version = (2, 0, 0)
 
 try:
     try:
