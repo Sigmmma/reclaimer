693a86365a334c1f0ed84978761c5f2abfb7876e
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-08-28 02:06:55 -0400

    Version bump

--- reclaimer/setup.py
+++ reclaimer/setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2018.08.27"
-version = (2, 0, 4)
+release_date = "2018.08.28"
+version = (2, 0, 5)
 
 try:
     try:
