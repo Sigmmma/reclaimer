1240e451b179871e78e391a709d79579072cd747
Moses_of_Egypt <Moses_of_Egypt@localhost>
2018-09-29 04:28:25 -0400

    Implementing extracting animations Removed experimental flags from antr Moved enumerators out of common_descs into their own enums.py

--- data_extraction.py
+++ data_extraction.py
@@ -38,6 +38,8 @@ from reclaimer.hek.defs.hmt_ import icon_types as hmt_icon_types
 from reclaimer.hsc import get_hsc_data_block, hsc_bytecode_to_string
 from reclaimer.model.jms import write_jms, JmsModel, JmsNode,\
      JmsMaterial, JmsMarker, JmsVertex, JmsTriangle
+from reclaimer.animation.jma import JmaAnimation, JmaNode, JmaNodeState,\
+     write_jma, get_anim_ext
 
 #load the palette for p-8 bump maps
 P8_PALETTE = load_palette()
@@ -923,12 +925,179 @@ def extract_model(tagdata, tag_path, **kw):
                 markers, regions, verts, tris))
 
 
+def extract_animation(tagdata, tag_path, **kw):
+    filepath_base = join(kw['out_dir'], dirname(tag_path), "animations")
+    endian = "<" if kw.get('halo_map') else ">"
+    unpack_trans = PyStruct(endian + "3f").unpack
+    unpack_ijkw  = PyStruct(endian + "4h").unpack
+    unpack_dxdy  = PyStruct(endian + "2f").unpack
+    unpack_float = PyStruct(endian + "f").unpack
+    
+
+    anim_nodes = []
+    for node in tagdata.nodes.STEPTREE:
+        anim_nodes.append(JmaNode(node.name, node.first_child_node_index,
+                                  node.next_sibling_node_index))
+
+    if not anim_nodes:
+        print("Animation tag is missing its nodes. Cannot extract animations.")
+        return
+
+    for anim in tagdata.animations.STEPTREE:
+        try:
+            anim_type = anim.type.enum_name
+            frame_info_type = anim.frame_info_type.enum_name.lower()
+            world_relative = anim.flags.world_relative
+
+            has_dxdy = "dx,dy" in frame_info_type
+            has_dz   = "dz" in frame_info_type
+            has_dyaw = "dyaw" in frame_info_type
+
+            anim_ext = get_anim_ext(anim_type, frame_info_type, world_relative)
+
+            frame_count = anim.frame_count
+            node_count  = anim.node_count
+            trans_int = anim.trans_flags0 + (anim.trans_flags1 << 32)
+            rot_int   = anim.rot_flags0   + (anim.rot_flags1   << 32)
+            scale_int = anim.scale_flags0 + (anim.scale_flags1 << 32)
+
+            trans_flags = tuple(bool(trans_int & (1 << i)) for i in range(node_count))
+            rot_flags   = tuple(bool(rot_int   & (1 << i)) for i in range(node_count))
+            scale_flags = tuple(bool(scale_int & (1 << i)) for i in range(node_count))
+
+            frame_info   = anim.frame_info.STEPTREE
+            default_data = anim.default_data.STEPTREE
+            frame_data   = anim.frame_data.STEPTREE
+
+            anim_frames = []
+
+            frame_info_size = {
+                "dx,dy": 8,
+                "dx,dy,dyaw": 12,
+                "dx,dy,dz,dyaw": 16}.get(frame_info_type, 0) * frame_count
+            frame_size = (12 * sum(trans_flags) + 8 * sum(rot_flags) +
+                          4  * sum(scale_flags))
+            default_data_size = anim.node_count * (12 + 8 + 4) - frame_size
+            uncomp_data_size = frame_size * anim.frame_count
+            if len(anim_nodes) != anim.node_count:
+                print("Skipping animation with different number of nodes " +
+                      "than the tag contains: '%s'" % anim.name)
+                continue
+            elif frame_info_size > len(frame_info):
+                print("Skipping animation with less frame_info data " +
+                      "than it is expected to contain: '%s'" % anim.name)
+                continue
+            elif default_data_size > len(default_data):
+                print("Skipping animation with less default_data " +
+                      "than it is expected to contain: '%s'" % anim.name)
+                continue
+            elif anim.flags.compressed_data and uncomp_data_size > anim.offset_to_compressed_data:
+                print("Skipping animation without uncompressed " +
+                      "animation data: '%s'" % anim.name)
+                continue
+            elif uncomp_data_size > len(frame_data):
+                print("Skipping animation with less frame_data " +
+                      "than it is expected to contain: '%s'" % anim.name)
+                continue
+
+            # sum the frame info changes for each frame
+            finfo_i = 0
+            last_root_node_info = [0, 0, 0, 0]
+            root_node_infos = tuple([] for i in range(anim.frame_count))
+            for f in range(anim.frame_count):
+                dx = dy = dz = dyaw = 0.0
+                if has_dxdy:
+                    dx, dy = unpack_dxdy(frame_info[finfo_i: finfo_i + 8])
+                    finfo_i += 8
+
+                if has_dz:
+                    dz = unpack_float(frame_info[finfo_i: finfo_i + 4])[0]
+                    finfo_i += 4
+
+                if has_dyaw:
+                    dyaw = unpack_float(frame_info[finfo_i: finfo_i + 4])[0]
+                    finfo_i += 4
+
+                x, y, z, yaw = last_root_node_info
+                root_node_infos[f][:] = [x + dx, y + dy, z + dz, yaw + dyaw]
+                last_root_node_info = root_node_infos[f]
+
+
+            ddata_i = 0
+            if anim_type == "overlay":
+                node_states = []
+                anim_frames.append(node_states)
+                for n in range(anim.node_count):
+                    i = j = k = x = y = z = 0.0
+                    w = scale = 1.0
+                    if not trans_flags[n]:
+                        x, y, z = unpack_trans(
+                            default_data[ddata_i: ddata_i + 12])
+                        ddata_i += 12
+
+                    if not rot_flags[n]:
+                        i, j, k, w = unpack_ijkw(
+                            default_data[ddata_i: ddata_i + 8])
+                        i, j, k, w = i/32767, j/32767, k/32767, w/32767
+                        ddata_i += 8
+
+                    if not scale_flags[n]:
+                        scale = unpack_float(
+                            default_data[ddata_i: ddata_i + 4])[0]
+                        ddata_i += 4
+
+                    node_states.append(JmaNodeState(i, j, k, w, x, y, z, scale))
+
+
+            fdata_i = 0
+            for f in range(anim.frame_count):
+                node_states = []
+                anim_frames.append(node_states)
+                for n in range(anim.node_count):
+                    i = j = k = x = y = z = 0.0
+                    w = scale = 1.0
+                    if trans_flags[n]:
+                        x, y, z = unpack_trans(
+                            frame_data[fdata_i: fdata_i + 12])
+                        fdata_i += 12
+
+                    if rot_flags[n]:
+                        i, j, k, w = unpack_ijkw(
+                            frame_data[fdata_i: fdata_i + 8])
+                        i, j, k, w = i/32767, j/32767, k/32767, w/32767
+                        fdata_i += 8
+
+                    if scale_flags[n]:
+                        scale = unpack_float(
+                            frame_data[fdata_i: fdata_i + 4])[0]
+                        fdata_i += 4
+
+                    if n == 0:
+                        dx, dy, dz, dyaw = root_node_infos[f]
+                        x, y, z, = x + dx, y + dy, z + dz
+                        
+
+                    node_states.append(JmaNodeState(i, j, k, w, x, y, z, scale))
+
+            write_jma(
+                join(filepath_base, anim.name + anim_ext),
+                JmaAnimation(
+                    anim.name, anim.node_list_checksum,
+                    anim_type, frame_info_type, anim.flags.world_relative,
+                    anim_nodes, anim_frames)
+                )
+        except Exception:
+            print(format_exc())
+            print("Could not extract animation.")
+
+
 h1_data_extractors = {
     'phys': extract_physics,
     'mode': extract_model, 'mod2': extract_model,
-    #'coll', 'sbsp',
-    #'antr', 'magy',
-    #'font', 'unic',
+    'antr': extract_animation, 'magy': extract_animation,
+    #'coll': extract_collision, 'sbsp': extract_collision,
+    #'font',
+    #'unic',
     'str#': extract_string_list, 'ustr': extract_unicode_string_list,
     "bitm": extract_bitmaps, "snd!": extract_h1_sounds,
     "hmt ": extract_hud_message_text, 'scnr': extract_h1_scnr_data,
