049ffd4fb88d9024e3857051a5085d29e93aa708
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-11-16 02:36:23 -0500

    Moving modules in meta\objs in meta\wrappers as it went against convention Fixed bug in stripify Simplified hek\defs\objs\ustr Created a halo 1 resource map tag class and moved functions to add tags to them into it

---
+++ reclaimer/meta/halo1_rsrc_map.py
@@ -0,0 +1,46 @@
+from ..common_descs import *
+from .objs.halo1_rsrc_map import Halo1RsrcMapTag
+from supyr_struct.defs.tag_def import TagDef, BlockDef
+
+
+def get():
+    return halo1_rsrc_map_def
+
+
+def tag_path_pointer(parent=None, new_value=None, **kwargs):
+    if parent is None:
+        raise KeyError()
+    t_head = parent.parent
+    if new_value is None:
+        return t_head.parent.parent.tag_paths_pointer + t_head.path_offset
+    t_head.path_offset = new_value - t_head.parent.parent.tag_paths_pointer
+
+
+rsrc_tag = Container("tag",
+    BytesRaw("data", SIZE="..size", POINTER="..offset"),
+    CStrLatin1("path", POINTER=tag_path_pointer),
+    )
+
+tag_header = Struct("tag header",
+    LUInt32("path offset"),
+    LUInt32("size"),
+    LUInt32("offset"),
+    STEPTREE=rsrc_tag
+    )
+
+halo1_rsrc_map_def = TagDef("halo1_rsrc_map",
+    LUEnum32("resource type",
+        'NONE',
+        'bitmaps',
+        'sounds',
+        'strings'
+        ),
+    LPointer32("tag paths pointer"),
+    LPointer32("tag headers pointer"),
+    LUInt32("tag count"),
+    Array("tags",
+        SIZE='.tag_count', SUB_STRUCT=tag_header,
+        POINTER='.tag_headers_pointer',
+        ),
+    endian="<", ext=".map", tag_cls=Halo1RsrcMapTag
+    )
