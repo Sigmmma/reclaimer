d11459be64caf10f0ee56b39829e6234d989e335
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-12-08 22:30:07 -0500

    More work implementing gen 3 maps

---
+++ meta/wrappers/halo5_map.py
@@ -0,0 +1,5 @@
+from .halo3_map import Halo3Map
+
+class Halo5Map(Halo3Map):
+    tag_defs_module = ""
+    tag_classes_to_load = tuple()
---
+++ meta/wrappers/halo_reach_map.py
@@ -0,0 +1,5 @@
+from .halo3_map import Halo3Map
+
+class HaloReachMap(Halo3Map):
+    tag_defs_module = ""
+    tag_classes_to_load = tuple()
