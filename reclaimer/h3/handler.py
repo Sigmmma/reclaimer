d0094c27a7da0c88048d12c396c340110b96a61c
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2019-01-02 20:47:35 -0500

    Fixed h3 handler not using proper frozen import def names

--- reclaimer/h3/handler.py
+++ reclaimer/h3/handler.py
@@ -1,9 +1,12 @@
 import os
+
+from .defs import __all__ as all_def_names
 from ..hek.handler import HaloHandler
 from reclaimer.data_extraction import h3_data_extractors
 
 class Halo3Handler(HaloHandler):
-    default_defs_path = "reclaimer.h3.defs"
+    frozen_imp_paths = all_def_names
     tag_header_engine_id = "b3am"
+    default_defs_path = "reclaimer.h3.defs"
 
     tag_data_extractors = h3_data_extractors
