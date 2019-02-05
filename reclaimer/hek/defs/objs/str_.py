f855bd8a5316d46f6ddd979e74eb1436e87230c4
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2019-02-05 03:06:58 -0500

    Fixed bug with string lists not saving properly(please god let this be the last time)

--- reclaimer/hek/defs/objs/str_.py
+++ reclaimer/hek/defs/objs/str_.py
@@ -9,11 +9,12 @@ class Str_Tag(HekTag):
         for i in range(len(strings)):
             # replace all instances of \r and \n with \r\n
             split_strings = []
-            for s in strings[i].data.split('\r'):
-                split_strings.extend(s.split('\n'))
+            for s in strings[i].data.split("\r\n"):
+                for sub_s in s.split('\r'):
+                    split_strings.extend(sub_s.split('\n'))
 
             new_string = split_strings.pop(0)
             for s in split_strings:
-                new_string += s + "\r\n"
+                new_string += "\r\n" + s
 
             strings[i].data = new_string
