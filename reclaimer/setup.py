2ec1db86642be27c189c4fd851a4933966a94cee
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2019-02-05 23:05:32 -0500

    Fixed bug with cutscene up time being incorrect when extracting scenarios

--- reclaimer/setup.py
+++ reclaimer/setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2019.02.05"
-version = (2, 2, 6)
+release_date = "2019.02.06"
+version = (2, 2, 7)
 
 try:
     try:
