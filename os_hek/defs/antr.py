803aac7a734add8a31e49c7dfe549c0a261722f1
Moses_of_Egypt <Moses_of_Egypt@localhost>
2019-03-21 16:25:48 -0400

    Version bump Fixed bug with all open sauce antr not having correct upper limit for animation count

--- os_hek/defs/antr.py
+++ os_hek/defs/antr.py
@@ -1,1 +1,17 @@
 from ...hek.defs.antr import *
+
+antr_body = desc_variant(
+    antr_body,
+    ("animations", reflexive(
+        "animations", animation_desc, 2048, DYN_NAME_PATH=".name"))
+    )
+
+def get():
+    return antr_def
+
+antr_def = TagDef("antr",
+    blam_header('antr', 4),
+    antr_body,
+
+    ext=".model_animations", endian=">", tag_cls=AntrTag
+    )
