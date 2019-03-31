7aa03a0e07273442efc9c225d3ec692136672a4d
Moses_of_Egypt <Moses_of_Egypt@localhost>
2019-03-31 18:41:43 -0400

    Updated Mod2Tag.globalize_local_markers so markers end up sorted how halo's picky ass wants them to be

--- __init__.py
+++ __init__.py
@@ -3,8 +3,8 @@
 # ##############
 __author__ = "Devin Bobadilla"
 #   #           YYYY.MM.DD
-__date__ = "2019.03.21"
-__version__ = (2, 3, 0)
+__date__ = "2019.03.31"
+__version__ = (2, 3, 1)
 __all__ = (
     "h2", "h3", "hek", "os_hek", "os_v3_hek", "os_v4_hek", "stubbs",
     "meta", "misc", "adpcm", "common_descs", "constants", "data_extraction",
