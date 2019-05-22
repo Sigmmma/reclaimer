236bd714c698f316976cfb1f42260750c7e88343
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2019-05-22 02:39:20 -0400

    Moved several functions from sbsp to mod2 converter into reclaimer

--- model/jms.py
+++ model/jms.py
@@ -1201,3 +1201,53 @@ def write_jms(filepath, jms_data):
                 tri.v0, tri.v1, tri.v2
                 )
             )
+
+
+def edge_loop_to_strippable_tris(edge_loop, region=0, mat_id=0):
+    tris = [None] * (len(edge_loop) - 2)
+    vert_ct = len(edge_loop)
+
+    even_face_ct = (vert_ct - 1) // 2
+    odd_face_ct = (vert_ct - 2) // 2
+
+    # make the even faces
+    v0 = edge_loop[0]
+    for i in range(even_face_ct):
+        v1 = edge_loop[i + 1]
+        v2 = edge_loop[vert_ct - 1 - i]
+        tris[i << 1] = JmsTriangle(region, mat_id, v0, v1, v2)
+
+        v0 = v2
+
+    # make the odd faces
+    v0 = edge_loop[1]
+    for i in range(odd_face_ct):
+        v1 = edge_loop[i + 2]
+        v2 = edge_loop[vert_ct - 1 - i]
+        tris[(i << 1) + 1] = JmsTriangle(region, mat_id, v0, v1, v2)
+
+        v0 = v1
+
+    return tris
+
+
+def edge_loop_to_fannable_tris(edge_loop, region=0, mat_id=0):
+    vert_index_count = len(edge_loop)
+    v0 = edge_loop[0]
+    return [JmsTriangle(region, mat_id, v0,
+                        edge_loop[((i + 1) % vert_index_count)],
+                        edge_loop[((i + 2) % vert_index_count)])
+            for i in range(len(edge_loop) - 2)]
+
+
+def edge_loop_to_tris(edge_loop_or_vert_index_count, region=0, mat_id=0,
+                      base=0, make_fan=False):
+    if isinstance(edge_loop_or_vert_index_count, int):
+        edge_loop = list(range(base, base + edge_loop_or_vert_index_count))
+    else:
+        edge_loop = edge_loop_or_vert_index_count
+
+    if make_fan:
+        return edge_loop_to_fannable_tris(edge_loop, region, mat_id)
+
+    return edge_loop_to_strippable_tris(edge_loop, region, mat_id)
