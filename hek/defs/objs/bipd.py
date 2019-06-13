7109c0e314bb2fc82c7b3325dfb4fad4715c1dd9
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2019-06-12 20:03:37 -0400

    fixed a few incorrect field names

--- hek/defs/objs/bipd.py
+++ hek/defs/objs/bipd.py
@@ -14,8 +14,8 @@ class BipdTag(ObjeTag):
         physics.cosine_stationary_turning_threshold = cos(bipd_attrs.stationary_turning_threshold)
 
         physics.cosine_maximum_slope_angle = cos(movement.maximum_slope_angle)
-        physics.negative_sine_downhill_falloff_angle = -sin(movement.downhill_falloff_angle)
-        physics.negative_sine_downhill_cutoff_angle  = -sin(movement.downhill_cutoff_angle)
+        physics.neg_sine_downhill_falloff_angle = -sin(movement.downhill_falloff_angle)
+        physics.neg_sine_downhill_cutoff_angle  = -sin(movement.downhill_cutoff_angle)
         physics.sine_uphill_falloff_angle = sin(movement.uphill_falloff_angle)
         physics.sine_uphill_cutoff_angle  = sin(movement.uphill_cutoff_angle)
 
