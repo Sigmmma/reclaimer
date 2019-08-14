ee7a60ace6a46de4fa0af6021c2251c3b196e02e
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2019-08-14 15:12:47 -0400

    version bump

--- reclaimer/__init__.py
+++ reclaimer/__init__.py
@@ -3,8 +3,8 @@
 # ##############
 __author__ = "Devin Bobadilla"
 #   #           YYYY.MM.DD
-__date__ = "2019.08.10"
-__version__ = (2, 4, 3)
+__date__ = "2019.08.14"
+__version__ = (2, 4, 4)
 __all__ = (
     "animation", "bitmaps", "h2", "h3", "halo_script", "hek", "meta", "misc",
     "model", "os_hek", "os_v3_hek", "os_v4_hek", "physics",
