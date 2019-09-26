43b3e67c520ef48f384411973793cddd8e71f526
gbMichelle <gbmichelle.dev@gmail.com>
2019-09-26 20:57:31 +0200

    Make it so there is only one function for full tagpaths.

--- reclaimer/util/path.py
+++ reclaimer/util/path.py
@@ -14,84 +14,61 @@ from pathlib import Path, PurePath, PurePosixPath, PureWindowsPath
 import os
 from os import path
 
-# Inverted this comparison so the version with the most
-# important code is at the top.
-if os.name != 'nt':
-    # If not windows then we're likely on a posix filesystem.
-    # This function will not break on windows. But it's just slower.
-    def tagpath_to_fullpath(tagdir, tagpath, extension="", force_windows=False, folder=False):
-        '''Takes a tagpath and case-insenstively goes through the directory
-        tree to find the true path if it exists. (True path being the path with
-        proper capitalization.) If force_windows is True, it will always treat
-        the path as a windows path, otherwise it will treat it as whatever
-        operating system you are using.
+# If not windows then we're likely on a posix filesystem.
+# This function will not break on windows. But it's just slower.
+def tagpath_to_fullpath(tagdir, tagpath, extension="", force_windows=False, folder=False):
+    '''Takes a tagpath and case-insenstively goes through the directory
+    tree to find the true path if it exists. (True path being the path with
+    proper capitalization.) If force_windows is True, it will always treat
+    the path as a windows path, otherwise it will treat it as whatever
+    operating system you are using.
 
-        Tagpaths from saved tagfiles should always be treated as windows.
-        Tagpaths from filepickers must be native, and thus not forced_windows.
+    Tagpaths from saved tagfiles should always be treated as windows.
+    Tagpaths from filepickers must be native, and thus not forced_windows.
 
-        If folder is True this program will search for a folder and assume
-        that the path does not contain a file at the end.
+    If folder is True this program will search for a folder and assume
+    that the path does not contain a file at the end.
 
-        Returns properly capitalized path if found. None if not found.'''
+    Returns properly capitalized path if found. None if not found.'''
 
-        if tagdir == "" or tagpath == "":
+    if tagdir == "" or tagpath == "":
+        return None
+    # Get all elements of the tagpath
+    if force_windows:
+        tagpath = list(pathlib.PureWindowsPath(tagpath).parts)
+    else:
+        tagpath = list(pathlib.PurePath(tagpath).parts)
+    # Get the final element: The tag!
+    tagname = ""
+    if not folder:
+        tagname = (tagpath.pop(-1) + extension).lower()
+    # Store our current progression through the tree.
+    cur_path = tagdir
+    for dir in tagpath:
+        subdirs = os.listdir(cur_path) # Get all files in the current dir
+        found = False
+        # Check if there is directories with the correct name
+        for subdir in subdirs:
+            if (subdir.lower() == dir.lower()):
+                fullpath = path.join(cur_path, subdir)
+                if not path.isdir(fullpath):
+                    continue
+                # Add the current directory to the end of our full path.
+                cur_path = fullpath
+                found = True
+                break
+        # If no matching directory was found, give up.
+        if not found:
             return None
-        # Get all elements of the tagpath
-        if force_windows:
-            tagpath = list(pathlib.PureWindowsPath(tagpath).parts)
-        else:
-            tagpath = list(pathlib.PurePath(tagpath).parts)
-        # Get the final element: The tag!
-        tagname = ""
-        if not folder:
-            tagname = (tagpath.pop(-1) + extension).lower()
-        # Store our current progression through the tree.
-        cur_path = tagdir
-        for dir in tagpath:
-            subdirs = os.listdir(cur_path) # Get all files in the current dir
-            found = False
-            # Check if there is directories with the correct name
-            for subdir in subdirs:
-                if (subdir.lower() == dir.lower()):
-                    fullpath = path.join(cur_path, subdir)
-                    if not path.isdir(fullpath):
-                        continue
-                    # Add the current directory to the end of our full path.
-                    cur_path = fullpath
-                    found = True
-                    break
-            # If no matching directory was found, give up.
-            if not found:
-                return None
         # Check if we can find the right file at the end of the chain
-        if not folder:
-            files = os.listdir(cur_path) # Get all files in the current dir
-            for file in files:
-                fullpath = os.path.join(cur_path, file)
-                if file.lower() == tagname and os.path.isfile(fullpath):
-                    return fullpath
-        # If the execution reaches this point, nothing is found.
-        return None
-
-else:
-    # We use this if on windows.
-    def tagpath_to_fullpath(tagdir, tagpath, extension="", force_windows=False):
-        '''Takes a tagpath and checks if it is valid. This is the windows
-        version, and on windows anything goes in terms of case sensitivity.
-        So, that's why it's simpler than the posix version.
-
-        Returns all lowercase path if found. None if not found.'''
-
-        # Get a full path.
-        tagdir = os.path.normpath(tagdir).lower()
-        tagpath = os.path.normpath(tagpath).lower() + extension.lower()
-        fullpath = os.path.join(tagdir, tagpath)
-        # Check if there is a file at this path.
-        if os.path.isfile(fullpath):
-            return fullpath
-        # If the execution reaches this point, nothing is found.
-        return None
-
+    if not folder:
+        files = os.listdir(cur_path) # Get all files in the current dir
+        for file in files:
+            fullpath = os.path.join(cur_path, file)
+            if file.lower() == tagname and os.path.isfile(fullpath):
+                return fullpath
+    # If the execution reaches this point, nothing is found.
+    return None
 
 def path_split(path, splitword, force_windows=False):
     '''Takes a path and case-insentively splits it to
@@ -119,7 +96,7 @@ def path_split(path, splitword, force_windows=False):
 
     return str(pathlib.PurePath(new_path))
 
-def path_replace(path, replace, new, backwards=True):
+def path_replace(path, replace, new, backwards=True, split=False):
     '''Case-insentively replaces a part of the given path.
     Checks what pieces exist in the replaced string and will math the new path
     up to the existing point and finishes it with whatever was put in if it
@@ -145,7 +122,8 @@ def path_replace(path, replace, new, backwards=True):
     before_parts.extend(parts[:split_idx])
     # Start after parts at the replacement point.
     after_parts = [new]
-    after_parts.extend(parts[split_idx+1:])
+    if not split:
+        after_parts.extend(parts[split_idx+1:])
 
     # Go through each directory level and find the corresponding directory name
     # case insensitively. Give up if we can't find any.
@@ -182,4 +160,8 @@ def path_replace(path, replace, new, backwards=True):
 
 def path_normalize(path):
     '''Normalizes a path: Removes redundant seperators, and lower cases it on Windows.'''
+    # Handling an edge case here. If a path is empty it will turn into "."
+    # Which will fuck up some 'not' operators.
+    if path == "":
+        return path
     return os.path.normpath(os.path.normcase(path))
