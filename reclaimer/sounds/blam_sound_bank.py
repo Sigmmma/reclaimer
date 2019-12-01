e768fca4c689062ffe7eb3bfdafc9cb4c82205e6
Moses_of_Egypt <mosesbobadilla@gmail.com>
2019-12-01 03:20:44 -0500

    Made module for intermediate sound bank class. Moved save_sound_perms into blam_sound_bank for refactoring into pipeline

---
+++ reclaimer/sounds/blam_sound_bank.py
@@ -0,0 +1,84 @@
+import os
+import re
+
+from traceback import format_exc
+from supyr_struct.defs.audio.wav import wav_def
+from reclaimer.sounds.adpcm import ADPCM_BLOCKSIZE, PCM_BLOCKSIZE
+
+BAD_PATH_CHAR_REMOVAL = re.compile(r'[<>:"|?*]{1, }')
+
+
+class BlamSoundPermutation:
+    pass
+
+
+class BlamSoundPitchRange:
+    pass
+
+
+class BlamSoundBank:
+    pass
+
+
+def write_blam_sound_bank_permutation_list(
+        permlist, filepath_base, sample_rate, channels=1, overwrite=True):
+    wav_file = wav_def.build()
+
+    for i in range(len(permlist)):
+        encoding, samples = permlist[i]
+        filepath = filepath_base
+        if not samples:
+            continue
+
+        if len(permlist) > 1: filepath += "__%s" % i
+
+        if encoding in ("ogg", "wma"):
+            filepath += ".%s" % encoding
+        elif not encoding:
+            filepath += ".bin"
+        else:
+            filepath += ".wav"
+
+        filepath = BAD_PATH_CHAR_REMOVAL.sub("_", filepath)
+
+        if not overwrite and os.path.isfile(filepath):
+            continue
+
+        if encoding in ("ogg", "wma") or not encoding:
+            try:
+                folderpath = os.path.dirname(filepath)
+                # If the path doesnt exist, create it
+                if not os.path.exists(folderpath):
+                    os.makedirs(folderpath)
+
+                with open(filepath, "wb") as f:
+                    f.write(samples)
+            except Exception:
+                print(format_exc())
+        else:
+            wav_file.filepath = filepath
+
+            wav_fmt = wav_file.data.format
+            wav_fmt.channels = channels
+            wav_fmt.sample_rate = sample_rate
+            wav_fmt.bits_per_sample = 16
+            wav_fmt.byte_rate = ((wav_fmt.sample_rate *
+                                  wav_fmt.bits_per_sample *
+                                  wav_fmt.channels) // 8)
+
+            samples_len = len(samples)
+            if encoding == "none":
+                wav_fmt.fmt.set_to('pcm')
+                wav_fmt.block_align = 2 * wav_fmt.channels
+            else:
+                wav_fmt.fmt.set_to('ima_adpcm')
+                wav_fmt.block_align = XBOX_ADPCM_ENCODED_BLOCKSIZE * wav_fmt.channels
+                wav_fmt.byte_rate = int(wav_fmt.byte_rate *
+                                        XBOX_ADPCM_ENCODED_BLOCKSIZE/XBOX_ADPCM_DECODED_BLOCKSIZE/2)
+
+            wav_file.data.wav_data.audio_data = samples
+            wav_file.data.wav_data.audio_data_size = samples_len
+            wav_file.data.wav_header.filesize = 36 + samples_len
+
+            wav_file.serialize(temp=False, backup=False)
+
