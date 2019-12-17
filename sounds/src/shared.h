6cbc2e876ffcca746cbf197e9a455340e43bf619
Moses_of_Egypt <mosesbobadilla@gmail.com>
2019-12-16 20:55:00 -0500

    Bumping adpcm encoder to work with up to MAX_AUDIO_CHANNEL_COUNT channels

--- sounds/src/shared.h
+++ sounds/src/shared.h
@@ -15,8 +15,6 @@ typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
 
-#define MAX_AUDIO_CHANNEL_COUNT 8
-
 #define RELEASE_PY_BUFFER_ARRAY(bufs)\
     for (int py_buffer_release_index = 0; py_buffer_release_index < (sizeof(bufs) / sizeof(bufs[0])); py_buffer_release_index++) {\
         PyBuffer_Release(&bufs[py_buffer_release_index]);\
