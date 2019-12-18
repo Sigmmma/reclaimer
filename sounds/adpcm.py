037dbecc090162c2eeb0defe0f14434dbcabca4d
Moses_of_Egypt <mosesbobadilla@gmail.com>
2019-12-18 04:02:37 -0500

    Removing unused import and mapping Starting to make adpcm module work with ima as well(in case halo can actually play it)

--- sounds/adpcm.py
+++ sounds/adpcm.py
@@ -2,8 +2,6 @@ import array
 import audioop
 import sys
 
-from struct import Struct as PyStruct
-
 try:
     from .ext import adpcm_ext
     fast_adpcm = True
@@ -15,12 +13,15 @@ __all__ = (
     "encode_adpcm_samples",
     )
 
-NIBBLE_SWAP_MAPPING = tuple(((val&15)<<4) | (val>>4) for val in range(256))
-
 # Byte size of an encoded xbox adpcm block
 XBOX_ADPCM_ENCODED_BLOCKSIZE = 36
 # Byte size of a decoded xbox adpcm block
 XBOX_ADPCM_DECODED_BLOCKSIZE = 128
+
+# Byte size of an encoded ima adpcm block
+IMA_ADPCM_ENCODED_BLOCKSIZE = 36
+# Byte size of a decoded ima adpcm block
+IMA_ADPCM_DECODED_BLOCKSIZE = 128
 
 
 NOISE_SHAPING_OFF     = 0  # flat noise (no shaping)
@@ -36,10 +37,14 @@ def decode_adpcm_samples(in_data, channel_ct, use_xbadpcm,
         raise NotImplementedError(
             "Accelerator module not detected. Cannot decompress ADPCM.")
 
+    encoded_blocksize = (XBOX_ADPCM_ENCODED_BLOCKSIZE if use_xbadpcm else
+                         IMA_ADPCM_ENCODED_BLOCKSIZE)
+    decoded_blocksize = (XBOX_ADPCM_DECODED_BLOCKSIZE if use_xbadpcm else
+                         IMA_ADPCM_DECODED_BLOCKSIZE)
+
     # divide by 2 since we're treating out_data as a sint16 iterable
     out_data = array.array("h", (0,)) * (
-        (len(in_data) // XBOX_ADPCM_ENCODED_BLOCKSIZE) *
-        (XBOX_ADPCM_DECODED_BLOCKSIZE // 2))
+        (len(in_data) // encoded_blocksize) * (decoded_blocksize // 2))
 
     adpcm_ext.decode_adpcm_samples(
         in_data, out_data, channel_ct, bool(use_xbadpcm))
@@ -61,18 +66,20 @@ def encode_adpcm_samples(in_data, channel_ct, use_xbadpcm,
     if (sys.byteorder == "big") != input_big_endian:
         out_data = audioop.byteswap(out_data, 2)
 
-    pcm_blocksize   = channel_ct * XBOX_ADPCM_DECODED_BLOCKSIZE
-    adpcm_blocksize = channel_ct * XBOX_ADPCM_ENCODED_BLOCKSIZE
+    encoded_blocksize = (XBOX_ADPCM_ENCODED_BLOCKSIZE if use_xbadpcm else
+                         IMA_ADPCM_ENCODED_BLOCKSIZE) * channel_ct
+    decoded_blocksize = (XBOX_ADPCM_DECODED_BLOCKSIZE if use_xbadpcm else
+                         IMA_ADPCM_DECODED_BLOCKSIZE) * channel_ct
 
-    pad_size = len(in_data) % pcm_blocksize
+    pad_size = len(in_data) % decoded_blocksize
     if pad_size:
-        pad_size = XBOX_ADPCM_DECODED_BLOCKSIZE - pad_size
+        pad_size = decoded_blocksize - pad_size
         # repeat the last sample to the end to pad to a multiple of blocksize
         pad_piece_size = (channel_ct * 2)
         in_data += in_data[-pad_piece_size: ] * (pad_size // pad_piece_size)
 
     out_data = bytearray(
-        (len(in_data) // pcm_blocksize) * adpcm_blocksize
+        (len(in_data) // decoded_blocksize) * encoded_blocksize
         )
 
     adpcm_ext.encode_adpcm_samples(
