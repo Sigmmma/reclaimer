e868ecbe3dcde9e0bcf19bb3b12fc3296c0e04e1
gbMichelle <gbmichelle.dev@gmail.com>
2020-01-04 01:14:05 +0100

    Add ISC license notice to the top of stripify

--- reclaimer/model/stripify.py
+++ reclaimer/model/stripify.py
@@ -1,3 +1,21 @@
+# Module taken from https://github.com/MosesofEgypt/snippets
+#
+# ISC License (ISC)
+#
+# Copyright 2017 Devin Bobadilla
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+# IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 import os
 
 from math import sqrt
@@ -346,7 +364,7 @@ class Stripifier():
             make_tuple = (hasattr(tris[0], "__getitem__") and
                           hasattr(tris[0][0], "__iter__") and
                           not isinstance(tris[0][0], tuple))
-            
+
             for src_tri in tris:
                 if make_tuple:
                     v0 = (tuple(src_tri[0]), tex_index)
