21ed29f7f5280c3df0b4c686a22792f6f105f624
gbMichelle <gbmichelle.dev@gmail.com>
2020-01-11 02:57:51 +0100

    Add documentation to sounds/ogg.py

--- sounds/ogg.py
+++ sounds/ogg.py
@@ -9,20 +9,9 @@
 
 class VorbisBitrateInfo:
     '''
-    Meant for storing bitrate info to pass to vorbis compression functions.
+    Intermediary class for storing bitrate info to pass to vorbis compression
+    functions.
     '''
-    # The below bitrate declarations are *hints*.
-    #   Combinations of the three values carry the following implications:
-    #    all three set to the same value:
-    #      implies a fixed rate bitstream
-    #    only nominal set:
-    #      implies a VBR stream that nominals the nominal bitrate.  No hard
-    #      upper/lower limit
-    #    upper and or lower set:
-    #      implies a VBR bitstream that obeys the bitrate limits. nominal
-    #      may also be set to give a nominal rate.
-    #    none set:
-    #      the coder does not care to speculate.
     lower = -1
     upper = -1
     nominal = -1
@@ -33,21 +22,46 @@ class VorbisBitrateInfo:
     use_quality = False
 
     def __init__(self, nominal=-1, lower=-1, upper=-1, quality=0.5):
+        '''
+        Combinations of nominal, lower, upper values carry the following
+        implications:
+          all three set to the same value:
+            implies a fixed rate bitstream
+          only nominal set:
+            implies a VBR stream that nominals the nominal bitrate.  No hard
+            upper/lower limit
+          upper and or lower set:
+            implies a VBR bitstream that obeys the bitrate limits. nominal
+            may also be set to give a nominal rate.
+          none set:
+            the coder does not care to speculate.
+        '''
         if quality is not None:
             self.set_bitrate_quality(quality)
         else:
             self.set_bitrate_variable(nominal, upper, lower)
 
     def set_bitrate_fixed(self, bitrate):
+        '''
+        Sets a fixed bitrate to the requested number.
+        '''
         self.upper = self.nominal = self.lower = nominal
         self.use_quality = False
 
     def set_bitrate_variable(self, nominal, upper=-1, lower=-1):
+        '''
+        Sets a variable bitrate based on the nominal and
+        optionally upper and lower numbers.
+        Check class initializer for further detail.
+        '''
         self.nominal = nominal
         self.upper = upper
         self.lower = lower
         self.use_quality = False
 
     def set_bitrate_quality(self, quality):
+        '''
+        Sets a bitrate using the quality float.
+        '''
         self.quality = min(1.0, max(-0.1, float(quality)))
         self.use_quality = True
