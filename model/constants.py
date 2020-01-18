9a79a2b343117bed6463583b4d99c7ec117db807
gbMichelle <gbmichelle.dev@gmail.com>
2020-01-18 03:43:16 +0100

    Add comments to model constants.

--- model/constants.py
+++ model/constants.py
@@ -6,9 +6,15 @@
 # Reclaimer is free software under the GNU General Public License v3.0.
 # See LICENSE for more information.
 #
-HALO_1_MAX_MATERIALS = 256
+
+# There is only 8 spaces allocated for these in the object's gamestate.
+# If you have more and it updates the state for those it would write into
+# unrelated fields.
 HALO_1_MAX_REGIONS = 8
 HALO_1_NAME_MAX_LEN = 31
+# These two numbers are the same because there shouldn't be more materials than
+# geometries. A geometry can only have one material after all.
+HALO_1_MAX_MATERIALS = 256
 HALO_1_MAX_GEOMETRIES_PER_MODEL = 256
 
 # If a jms file is prefixed with this token it
