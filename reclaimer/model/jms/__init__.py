eba92cda2b3c45e6cf341892c035f08f74dd318a
gbMichelle <gbmichelle.dev@gmail.com>
2020-01-30 15:23:02 +0100

    Split jms into seperate modules for maintainability. Use __all__ metavars to avoid namespace clutter.

---
+++ reclaimer/model/jms/__init__.py
@@ -0,0 +1,20 @@
+#
+# This file is part of Reclaimer.
+#
+# For authors and copyright check AUTHORS.TXT
+#
+# Reclaimer is free software under the GNU General Public License v3.0.
+# See LICENSE for more information.
+#
+
+from .file import *
+from .marker import *
+from .material import *
+from .merged_model import *
+from .merged_region import *
+from .mesh import *
+from .model import *
+from .node import *
+from .triangle import *
+from .vertex import *
+from . import util
