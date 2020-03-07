24f50c29fc2d1b463519cdfd7ce6052a653718c7
gbMichelle <gbmichelle.dev@gmail.com>
2020-03-07 21:06:34 +0100

    Calculate crc of a tag on serialization.

--- reclaimer/hek/defs/objs/tag.py
+++ reclaimer/hek/defs/objs/tag.py
@@ -6,21 +6,68 @@
 # Reclaimer is free software under the GNU General Public License v3.0.
 # See LICENSE for more information.
 #
+import zlib
+
+from pathlib import Path
 
 from supyr_struct.defs.constants import DEFAULT
 from supyr_struct.tag import Tag
+
+def calc_halo_crc32(buffer, offset=None, size=None, crc=0xFFffFFff):
+    if offset is not None:
+        buffer.seek(offset)
+
+    return zlib.crc32(buffer.read(size), crc ^ 0xFFffFFff) ^ 0xFFffFFff
 
 class HekTag(Tag):
     def __init__(self, **kwargs):
         self.calc_pointers = False
         Tag.__init__(self, **kwargs)
 
+    def serialize(self, **kwargs):
+        '''
+        Overload of the supyr serialization function that retroactively adds
+        a CRC to the tag.
+        '''
+        filepath = kwargs.get('filepath', self.filepath)
+        buffer = kwargs.get('buffer', None)
+
+        # Run the normal serialization.
+        result = Tag.serialize(self, **kwargs)
+
+        # If there is neither a buffer or filepath just return the result.
+        if (buffer is None) and (not filepath):
+            return result
+
+        # Prefer to use the buffer as that is how Tag.serialize does it.
+        f = buffer
+        if buffer is None:
+            f = Path(filepath).open('rb+')
+
+        # Calculate the crc from the offset 64 to the end. 64 is the size of
+        # the header which is skipped in this calculation.
+        crc = calc_halo_crc32(f, 64)
+        # Write the crc to offset 40 in the buffer.
+        # Matches up with self.data.blam_header.checksum.
+        f.seek(40)
+        f.write(crc.to_bytes(4, byteorder='big', signed=False))
+        # Flush the stream.
+        f.flush()
+        # Only close if it is a file. Because the only case where we own
+        # this buffer is if there was no buffer kwarg.
+        if not buffer:
+            f.close()
+
+        # Update the tag object so it won't have to be deserialized again.
+        self.data.blam_header.checksum = crc
+        return result
+
     def calc_internal_data(self):
         # recalculate the header data
         head = self.data.blam_header
 
         head.tag_class.data = head.tag_class.get_desc(DEFAULT)
-        head.checksum = head.get_desc(DEFAULT, 'checksum')
+        head.flags.edited_with_mozz = True
         head.header_size = head.get_desc(DEFAULT, 'header_size')
         head.version = head.get_desc(DEFAULT, 'version')
         head.integrity0 = head.get_desc(DEFAULT, 'integrity0')
