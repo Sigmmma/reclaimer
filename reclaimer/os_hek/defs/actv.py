68e7d4e26df7ff02e9f4bdab1bf049193b02b2e8
Moses_of_Egypt <mosesbobadilla@gmail.com>
2020-06-13 01:47:20 -0400

    Fix OS actv definition assuming incorrect grenade struct index

--- reclaimer/os_hek/defs/actv.py
+++ reclaimer/os_hek/defs/actv.py
@@ -11,13 +11,13 @@ from ...hek.defs.actv import *
 
 # TODO use some supyr trickery to make this nicer.
 
-# grenades descriptor index in actv_body is 10
+# grenades descriptor index in actv_body is 11
 # grenade_type descriptor index in grenades_desc is 1
 
 # replace the grenade_type descriptor with one
 # that uses open sauce's extra grenade slots
 actv_body = dict(actv_body)
-grenades_desc = actv_body[10] = dict(actv_body[10])
+grenades_desc = actv_body[11] = dict(actv_body[11])
 grenades_desc[1] = SEnum16("grenade_type", *grenade_types_os)
 
 def get():
