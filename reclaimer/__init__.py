04d2737732a56bc8d7b91638f577a909d9ba1b61
gbMichelle <gbmichelle.dev@gmail.com>
2020-07-06 07:47:38 +0200

    Bump version

--- reclaimer/__init__.py
+++ reclaimer/__init__.py
@@ -12,8 +12,8 @@
 # ##############
 __author__ = "Sigmmma"
 #           YYYY.MM.DD
-__date__ = "2020.06.13"
-__version__ = (2, 9, 4)
+__date__ = "2020.07.06"
+__version__ = (2, 9, 5)
 __website__ = "https://github.com/Sigmmma/reclaimer"
 __all__ = (
     "animation", "bitmaps", "h2", "h3", "halo_script", "hek", "meta", "misc",
