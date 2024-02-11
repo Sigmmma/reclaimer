134c7e1879af0ea6781753cf21d95636351799cf
Steven Garcia <stevengarcia55@ymail.com>
2024-02-11 17:38:40 -0600

    bump version

--- reclaimer/__init__.py
+++ reclaimer/__init__.py
@@ -12,8 +12,8 @@
 # ##############
 __author__ = "Sigmmma"
 #           YYYY.MM.DD
-__date__ = "2024.02.10"
-__version__ = (2, 15, 2)
+__date__ = "2024.02.11"
+__version__ = (2, 15, 3)
 __website__ = "https://github.com/Sigmmma/reclaimer"
 __all__ = (
     "animation", "bitmaps", "h2", "h3", "halo_script", "hek", "meta", "misc",
